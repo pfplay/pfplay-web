0c45cc834d24ac0f1de5fabd9d14e2879de103a6
위성원 <sungwon@wiseong-wons-MacBook-Pro.local>
2022-11-13 15:25:46 +0900

    feat: next-auth google 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import { SessionProvider } from 'next-auth/react';
+
 import type { AppProps } from 'next/app';
 import '@styles/globals.css';
+import { SessionWithAuth } from 'type/auth';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: SessionWithAuth;
+}>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
