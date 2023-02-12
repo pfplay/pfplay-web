7dd9fef56ee69d94273736d4bbbe0f7668ae2fa2
asdfsdfas34 <asdfsdfas34@gmail.com>
2023-02-12 19:09:28 +0900

    [FE-3] 파티리스트
    * 파티리스트 퍼블리싱

---
+++ src/components/modal/MyProflieModal.tsx
@@ -0,0 +1,109 @@
+import Image from 'next/future/image'
+import Modal from 'react-modal'
+
+const customStyles: Modal.Styles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  },
+  content: {
+    width: 680,
+    // padding: '54px 27px 32px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'rgba(0, 0, 0, 0.85)',
+    border: '1px solid #2F2F2F',
+  },
+}
+
+interface IMyProfileModalProps {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const MyProfileModal = ({ isOpen, onClose }: IMyProfileModalProps) => {
+  return (
+    <Modal style={customStyles} isOpen={isOpen} ariaHideApp={false}>
+      {/* Header */}
+      <div className="flex justify-between mx-1">
+        <div>
+          <p className="text-[#DADADA] text-[21px]">내 프로필</p>
+        </div>
+        <div></div>
+        <div onClick={onClose}>
+          <Image
+            className="rounded-full"
+            src="/image/icn_close.png"
+            alt="close"
+            width={24}
+            height={24}></Image>
+        </div>
+      </div>
+      {/* Contents */}
+      <div className="grid grid-rows-3 grid-flow-col gap-4 mx-5">
+        <div className="row-span-3 mt-5 bg-[#DADADA]">Avartar Area</div>
+        <div className="col-span-2 mt-5 flex">
+          <div className="ml-5 mr-3 flex items-center">
+            <p className="text-[#DADADA] text-[21px]">Fendi</p>
+          </div>
+          <div className="flex items-center">
+            <Image
+              className="rounded-full"
+              src="/image/icn_edit.png"
+              alt="close"
+              width={20}
+              height={20}></Image>
+          </div>
+        </div>
+        <div className="col-span-2 ml-5">
+          <p className="text-[#DADADA] text-[21px]">
+            요즘 뉴진스 안듣는 사람 아직도 있나요? 플리 채우려 장르 안가리고 노래 수집 중
+          </p>
+        </div>
+        <div className="col-span-2"></div>
+      </div>
+      {/* Footer */}
+      <div className="flex mt-5 mx-5">
+        <div className="basis-1/4">
+          <button
+            className=""
+            style={{
+              borderWidth: '1px',
+              borderRadius: '4px',
+              borderBlockColor: 'red',
+              borderLeftColor: 'red',
+              borderRightColor: 'red',
+              boxSizing: 'border-box',
+              // borderColor: 'red',
+            }}>
+            <p className="text-[#F31F2C] text-[15px] mx-1 mt-1 mb-1">아바타 설정</p>
+          </button>
+        </div>
+        <div className="basis-1/4 flex items-center">
+          <p className="text-[#969696] text-[14px] mr-3">포인트</p>
+          <p className="text-[#DADADA] text-[14px]">76p</p>
+        </div>
+        <div className="basis-1/4 flex items-center">
+          <p className="text-[#969696] text-[14px] mr-3">가입일</p>
+          <p className="text-[#DADADA] text-[14px]">2022.12.02</p>
+        </div>
+        <div className="basis-1/4 flex justify-end">
+          <Image
+            className="rounded-full"
+            src="/image/rainbow-app-icon-small 2.png"
+            alt="close"
+            width={32}
+            height={32}></Image>
+        </div>
+      </div>
+    </Modal>
+  )
+}
---
+++ src/components/partylist/subparty.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from 'next'
+import Image from 'next/future/image'
+
+export const SubParty: NextPage = () => {
+  return (
+    <>
+      <div
+        className="col-span-3 mx-[8px] px-[7px] bg_blur"
+        style={{
+          backgroundImage:
+            'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7) ),url(/image/ariana.png)',
+          backgroundSize: 'cover',
+        }}>
+        <div className="flex flex-col">
+          <h3 className="text-[#DADADA] text-[21px] mt-3 ml-3">
+            연말느낌 방구석 크리스마스 플레이리스트방
+          </h3>
+          <div className="flex items-center">
+            <div className="">
+              <Image
+                className="mx-[10px] rounded"
+                src="/image/ariana.png"
+                alt="ariana"
+                width={100}
+                height={50}></Image>
+            </div>
+            <div className="mt-4 ml-5">
+              <p className="text-[#DADADA]">Ariana Grande - Santa Tell Me (Official Video)</p>
+            </div>
+          </div>
+          <hr></hr>
+          <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-2 text-[#FDFDFD] flex items-end">
+              <Image
+                className="mt-4 ml-2 pb-3"
+                src="/image/icn_person_outline.png"
+                alt="person"
+                width={16}
+                height={16}></Image>
+              <p className="text-[12px] pl-1 pb-3">48</p>
+            </div>
+            <div className="col-span-8 text-[#FDFDFD] flex items-end justify-items-start pb-2">
+              <Image
+                className="mt-4 mx-[5px] rounded-full"
+                src="/image/user1.png"
+                alt="person"
+                width={24}
+                height={24}></Image>
+              <Image
+                className="mt-4 mx-[5px] rounded-full"
+                src="/image/user2.png"
+                alt="person"
+                width={24}
+                height={24}></Image>
+              <Image
+                className="mt-4 mx-[5px] rounded-full"
+                src="/image/user3.jfif"
+                alt="person"
+                width={24}
+                height={24}></Image>
+            </div>
+            <div className="col-span-2 text-[#FDFDFD] flex items-end">
+              <Image
+                className="mt-4 mx-[5px] pb-2 rounded-full"
+                src="/image/icn_info_outline.png"
+                alt="person"
+                width={24}
+                height={24}></Image>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default SubParty
