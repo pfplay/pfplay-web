3a586a208305a6afc6f2a6ca32170da592d400db
athumb <uumj222@gmail.com>
2023-02-19 23:35:06 +0900

    style: 💄 tailwindcss color theme 설정 (#15)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const darkRedThemeColors = {
+  red500: '#990316',
+  red600: '#B41024',
+  red700: '#F31F2C',
+  red800: '#FF5B65',
+  redGradientStart: '#780808',
+  redGradientEnd: '#AE001F',
+  statesColor: '#EAEAEA',
+  gray50: '#111111',
+  gray100: '#1C1C1C',
+  gray200: '#2F2F2',
+  gray300: '#434343',
+  gray400: '#545454',
+  gray500: '#707070',
+  gray600: '#969696',
+  gray700: '#DADADA',
+  gray800: '#F5F5F5',
+  gray900: '#FDFDFD',
+  dim: 'rgba(0, 0, 0, 0.6)',
+}
+
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: darkRedThemeColors,
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
