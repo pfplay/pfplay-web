5e199e84fadede2a4e00e7c3edc627688004b02b
wangtae <wangtae0320@gmail.com>
2023-05-30 22:36:50 +0900

    feat: 🎸[FE-3-1] 아바타 설정 페이지 퍼블리싱 및 RainbowKit 지갑 연동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
 
 import { MantineProvider } from '@mantine/core'
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import type { AppProps } from 'next/app'
 import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
 function MyApp({
   Component,
@@ -12,12 +17,31 @@ function MyApp({
 }: AppProps<{
   session: Session
 }>) {
+  const { chains, publicClient } = configureChains(
+    [mainnet, polygon, optimism, arbitrum],
+    [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }), publicProvider()],
+  )
+
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains,
+  })
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  })
+
   return (
     <SessionProvider session={pageProps.session}>
       <MantineProvider>
-        <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
-          <Component {...pageProps} />
-        </ThirdwebProvider>
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains} theme={darkTheme()} modalSize="wide">
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
       </MantineProvider>
     </SessionProvider>
   )
