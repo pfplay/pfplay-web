9634733b139b686afe6303dbe89420f149b7d16d
gusujin <gusu@likelion.net>
2023-06-11 21:54:32 +0900

    feat: 🎸 내 프로필 모달창 퍼블리싱

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
 
+import MyProfile from '../modal/MyProfile'
 import { HeadSet } from './icon/HeadSet'
 
 const SideBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="flex flex-col justify-between absolute py-[28px] top-1/2 left-[40px] w-[88px] h-[224px] bg-[#111111] border-solid border-[#545454] border mr-[24px] transform -translate-y-1/2">
       {/* TODO: 프로필 이미지로 변경, href 추가 */}
-      <Link href="/" passHref>
+      <div onClick={() => setIsOpen(true)}>
         <div className="flex flex-col justify-center items-center gap-1">
           <Image
             src="https://picsum.photos/200/300"
@@ -18,13 +21,14 @@ const SideBar = () => {
           />
           <p className="text-[#969696] text-[12px]">내 프로필</p>
         </div>
-      </Link>
+      </div>
       <Link href="/" passHref>
         <div className="flex flex-col justify-center items-center gap-1">
           <HeadSet />
           <p className="text-[#969696] text-[12px]">플레이 리스트</p>
         </div>
       </Link>
+      <MyProfile isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </div>
   )
 }
