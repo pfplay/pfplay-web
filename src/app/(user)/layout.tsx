bca5ba520985fea3a69358149f7af52c1bc2b160
Jongok Yoon <yoonjongok1209@gmail.com>
2023-07-22 16:09:36 +0200

    refactor: 💡 Form validation 적용

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProfileEditLayout = ({ children }: React.PropsWithChildren) => {
-  return <main className='pt-[120px] px-[120px] bg-black'>{children}</main>;
+  return <main className='p-[120px] bg-black'>{children}</main>;
 };
 
 export default ProfileEditLayout;
--- src/components/_features/profile/ProfileSettings.tsx
+++ src/components/_features/profile/ProfileSettings.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import React from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import GoBackButton from '@/components/GoBackButton';
@@ -11,26 +11,24 @@ import { cn } from '@/lib/utils';
 const profileFormSchema = z.object({
   nickName: z
     .string()
+    .min(1)
     .max(16)
     .refine((value) => !/[\s!@#$%^&*()_+{}\[\]:;<>,.?~\-]/g.test(value)),
-  introduction: z.string().max(50, '50자 제한'),
+  introduction: z.string().min(1).max(50, { message: '50자 제한' }),
 });
 
 type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
 const ProfileSettings = () => {
-  // const [isProfileSet, setIsProfileSet] = useState(false);
-  const [nickNameLetterCount, setNickNameLetterCount] = useState(0);
-  const [introductionLetterCount, setIntroductionLetterCount] = useState(0);
-
   const router = useRouter();
 
   const {
-    register,
     handleSubmit,
-    formState: { errors },
+    watch,
+    control,
+    formState: { errors, isValid },
   } = useForm<ProfileFormValues>({
-    mode: 'onChange',
+    mode: 'all',
     resolver: zodResolver(profileFormSchema),
     defaultValues: {
       nickName: '',
@@ -39,85 +37,95 @@ const ProfileSettings = () => {
   });
 
   const handleFormSubmit: SubmitHandler<ProfileFormValues> = (data) => {
-    console.log(data);
+    console.log({ data });
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const currentTarget = e.currentTarget.name as keyof ProfileFormValues;
-
-    if (currentTarget === 'nickName') {
-      setNickNameLetterCount(e.target.value.length);
-    }
-
-    if (currentTarget === 'introduction') {
-      setIntroductionLetterCount(e.target.value.length);
-    }
-  };
+  const isBtnDisabled = Object.keys(errors).length > 0 || !isValid;
 
   return (
-    <form
-      onSubmit={handleSubmit(handleFormSubmit)}
-      className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-[1200px] bg-grey-10 flexCol justify-between items-center pt-10 pb-12 px-[60px] text-grey-3 gap-14'
-    >
+    <section className='min-w-[1000px] bg-grey-10 flexCol justify-between items-center mx-auto pt-10 px-[60px]'>
       <div className='self-start'>
         <GoBackButton text='당신은 누구신가요?' onClick={() => router.back()} />
       </div>
-      <section>
+      <form
+        onSubmit={handleSubmit(handleFormSubmit)}
+        className='w-full h-full flexCol justify-between items-center gap-20 pt-24 pb-12  text-grey-3 '
+      >
         {/* nickname */}
-        <article className={'flex justify-between gap-10 mb-[77px]'}>
-          <h3 className='text-lg'>
-            닉네임<span className='text-red-2'>*</span>
-          </h3>
-          <div className=' w-[536px] flex flex-col'>
-            <div className='relative'>
-              <input
-                maxLength={16}
-                {...register('nickName')}
-                placeholder='한글 8자, 영문 16자 제한/띄어쓰기, 특수문자 사용 불가'
-                onChange={(e) => handleInputChange(e)}
-                className='w-full bg-grey-8 rounded px-4 py-[13px] placeholder:text-[15px]'
+        <div>
+          <div className={'flex justify-between gap-10 mb-[77px]'}>
+            <h3 className='text-lg'>
+              닉네임<span className='text-red-2'>*</span>
+            </h3>
+            <div className='w-[536px] flexCol'>
+              <Controller
+                control={control}
+                name='nickName'
+                render={({ field }) => (
+                  <div className='relative'>
+                    <input
+                      {...field}
+                      maxLength={16}
+                      placeholder='한글 8자, 영문 16자 제한/띄어쓰기, 특수문자 사용 불가'
+                      className='w-full bg-grey-8 rounded px-4 py-[13px] placeholder:text-[15px]'
+                    />
+                    <p className='absolute right-4 top-1/2 transform -translate-y-1/2 text-grey-5'>
+                      <span className={cn(errors.nickName && 'text-red-2')}>
+                        {watch('nickName') ? watch('nickName').length : 0}
+                      </span>
+                      /16
+                    </p>
+                  </div>
+                )}
               />
-              <p className='absolute right-4 top-1/2 transform -translate-y-1/2 text-grey-5'>
-                <span className={cn(errors.nickName?.message && 'text-red-2')}>
-                  {nickNameLetterCount}
-                </span>
-                /16
-              </p>
+              {errors.nickName && (
+                <InputErrorMessage errorMessage='한글 8자, 영문 16자 제한/띄어쓰기, 특수문자 사용 불가' />
+              )}
             </div>
-            {errors.nickName?.message && (
-              <InputErrorMessage errorMessage='한글 8자, 영문 16자 제한/띄어쓰기, 특수문자 사용 불가' />
-            )}
           </div>
-        </article>
-        {/* introduction */}
-        <article className='flex justify-between'>
-          <h3 className='text-lg'>소개</h3>
-          <div className='relative w-[536px] flex flex-col'>
-            <textarea
-              maxLength={50}
-              rows={3}
-              className='w-full bg-grey-8 rounded pt-[13px] px-4'
-              {...register('introduction')}
-              placeholder='한/영 구분 없이 띄어쓰기 포함 50자 제한'
-              onChange={(e) => handleInputChange(e)}
-            />
-            <p className='absolute right-4 bottom-3 text-grey-5'>
-              <span className={cn(errors.introduction?.message && 'text-red-2')}>
-                {introductionLetterCount}
-              </span>
-              /50
-            </p>
-            {errors.introduction?.message && (
-              <InputErrorMessage errorMessage=' 한/영 구분 없이 띄어쓰기 포함 50자 제한' />
-            )}
+          {/* introduction */}
+          <div className='flex justify-between'>
+            <h3 className='text-lg'>소개</h3>
+            <div className='w-[536px] flexCol'>
+              <Controller
+                control={control}
+                name='introduction'
+                render={({ field }) => (
+                  <div className='relative'>
+                    <textarea
+                      {...field}
+                      maxLength={50}
+                      rows={3}
+                      placeholder='한/영 구분 없이 띄어쓰기 포함 50자 제한'
+                      className='w-full bg-grey-8 rounded pt-[13px] px-4'
+                    />
+                    <p className='absolute right-4 bottom-3 text-grey-5'>
+                      <span className={cn(errors.introduction && 'text-red-2')}>
+                        {watch('introduction') ? watch('introduction').length : 0}
+                      </span>
+                      /50
+                    </p>
+                  </div>
+                )}
+              />
+              {errors.introduction && (
+                <InputErrorMessage errorMessage='한/영 구분 없이 띄어쓰기 포함 50자 제한' />
+              )}
+            </div>
           </div>
-        </article>
-      </section>
-
-      <button className='self-end bg-[#1C1C1C] text-[#434343] font-bold-sm px-[96px] py-[12px]'>
-        Let&apos;s get in
-      </button>
-    </form>
+        </div>
+        <button
+          type='submit'
+          className={cn(
+            'self-end bg-grey-9 text-grey-7 font-bold-sm px-[96px] py-[12px]',
+            !isBtnDisabled && 'bg-gradient-red text-grey-1'
+          )}
+          disabled={isBtnDisabled}
+        >
+          Let&apos;s get in
+        </button>
+      </form>
+    </section>
   );
 };
 
