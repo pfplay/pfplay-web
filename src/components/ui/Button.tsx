2b8746cce4e8e2f8f65c1e85f6dcad561dbb0bc0
wangtae <wangtae0320@gmail>
2023-07-26 19:58:34 +0900

    style: 💄 storybook atom (button) 추가

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,25 +4,38 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  [
-    'flexRowCenter',
-    'rounded-[4px]',
-    'capitalize',
-    'text-grey-1',
-    'text-base',
-    'font-bold',
-    'border-transparent',
-  ],
+  ['flexRowCenter', 'rounded', 'capitalize', 'text-grey-50', 'border-transparent'],
   {
     variants: {
       intent: {
-        text: [],
-        primary: ['bg-gradient-red'],
-        secondary: ['bg-grey-8'],
-        outlined: ['border-[1px]', 'border-grey-5'],
+        // FIXME : primary hover시 bg-gradient 색상 변경이 아닌 bg color 변경으로 가능 여부 확인 필요
+        primary: ['bg-gradient-red', 'hover:bg-gradient-to-tr from-red-400 to-red-400'],
+        'primary-disabled': ['bg-grey-800', 'text-grey-600'],
+        'primary-outline': [
+          'text-red-400',
+          'border-solid',
+          'border',
+          'border-red-400',
+          'hover:border-red-200',
+          'hover:text-red-200',
+        ],
+        'primary-outline-disabled': ['text-grey-700', 'border-solid', 'border', 'border-grey-700'],
+        secondary: ['bg-grey-700', 'hover:bg-grey-600'],
+        'secondary-disabled': ['bg-grey-900', 'text-grey-700'],
+        'secondary-outline': ['border-solid', 'border', 'border-grey-600', 'hover:border-grey-400'],
+        'secondary-outline-disabled': [
+          'border-solid',
+          'border',
+          'text-grey-800',
+          'border-grey-800',
+        ],
       },
       size: {
-        base: ['px-[68px]', 'py-[10px]'],
+        xLarge: ['px-4', 'py-[13px]', 'text-[20px]/[130%]', 'font-bold'],
+        large: ['px-4', 'py-3', 'text-base', 'font-bold'],
+        medium: ['px-4', 'py-[6px]', 'text-base', 'font-normal'],
+        small: ['px-3', 'py-[5.5px]', 'text-sm/[150%]', 'font-normal'],
+        xSmall: ['px-2', 'py-[3.5px]', 'text-sm/[21px]', 'font-normal'],
       },
       fullWidth: {
         true: ['w-full'],
@@ -30,12 +43,12 @@ const buttonVariants = cva(
     },
     defaultVariants: {
       intent: 'primary',
-      size: 'base',
+      size: 'xLarge',
     },
     compoundVariants: [
       {
-        intent: ['primary', 'secondary', 'text', 'outlined'],
-        size: 'base',
+        intent: ['primary', 'primary-outline', 'secondary', 'secondary-outline'],
+        size: 'xLarge',
       },
     ],
   }
