9aeed80018d884b59b3602399eb8759c009dd745
Jongok Yoon <yoonjongok1209@gmail.com>
2023-07-29 18:45:20 +0200

    refactor: 💡 tab 패널 설정

--- src/components/_features/Avatar/AvatarBody.tsx
+++ src/components/_features/Avatar/AvatarBody.tsx
@@ -1,21 +1,19 @@
+import Image from 'next/image';
+
 const AvatarBody = () => {
   return (
     <>
-      <div
-        className='grid grid-cols-5 gap-5 mt-7 scrollbar-hide overflow-y-auto'
-        style={{ maxHeight: '500px' }}
-      >
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
-        <div className=' bg-gray-500' style={{ maxWidth: '200px', aspectRatio: 'auto 1/1' }}></div>
+      <div className='max-h-[400px] grid grid-cols-5 gap-5 mt-7 scrollbar-hide overflow-y-auto'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map((avatar) => (
+          // TODO: inject avatar src and alt
+          <Image
+            key={avatar}
+            src={''}
+            alt={''}
+            sizes='(max-width:200px)'
+            className='bg-grey-500 max-h-[200px] aspect-square'
+          />
+        ))}
       </div>
     </>
   );
--- src/components/_features/Avatar/AvatarSettings.tsx
+++ src/components/_features/Avatar/AvatarSettings.tsx
@@ -1,42 +1,54 @@
 'use client';
-import React from 'react';
+import React, { FC, useState } from 'react';
 import { Tab } from '@headlessui/react';
 import GoBackButton from '@/components/GoBackButton';
 import { cn } from '@/lib/utils';
+import AvatarBody from './AvatarBody';
+import AvatarFace from './AvatarFace';
+
+const avatarSettingTabConfig: Array<{ name: 'body' | 'face'; component: FC; index: number }> = [
+  { name: 'body', index: 0, component: AvatarBody },
+  { name: 'face', index: 1, component: AvatarFace },
+];
 
 const AvatarSettings = () => {
-  // const router = useRouter();
+  const [selectedIndex, setSelectedIndex] = useState(avatarSettingTabConfig[0].index);
+
+  console.log({ selectedIndex });
 
   return (
     <section className='min-w-[1000px] bg-grey-900  mx-auto pt-[46px] pb-12 px-[60px]'>
       <GoBackButton text='뭘 입고 놀아볼까요?' className='self-start' />
-      <div className='flex gap-[30px]'>
+      <div className='flex gap-[30px] '>
         {/* 아바타 미리보기 */}
         <div className='bg-black min-w-[300px] h-[520px]'>avatar preview</div>
         {/* 아이템 설정 */}
-        <div className='flex-col w-full bg-white'>
-          <Tab.Group>
+        <div className='flex-col w-full'>
+          <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
             <Tab.List className={cn('w-full flex space-x-1 rounded-xl p-1 max-w-xs')}>
-              {['BODY', 'FACE'].map((value) => (
+              {avatarSettingTabConfig.map((tab) => (
                 // TODO: Tab 컴포넌트 Atom으로 분리하기
                 <Tab
-                  key={value}
+                  key={tab.index}
                   className={({ selected }) =>
                     cn(
-                      'w-[101px] flexRowCenter py-3 px-6 border-b-[1px] text-xl font-bold leading-5  bg-transparent outline-none',
+                      'w-[101px] flexRowCenter py-3 px-6 border-b-[1px] text-xl font-bold leading-5  bg-transparent outline-none uppercase',
                       selected
                         ? 'text-red-400 shadow border-red-700'
                         : 'text-grey-500 border-grey-500 '
                     )
                   }
                 >
-                  {value}
+                  {tab.name}
                 </Tab>
               ))}
             </Tab.List>
             <Tab.Panels>
-              <Tab.Panel>Content 1</Tab.Panel>
-              <Tab.Panel>Content 2</Tab.Panel>
+              {avatarSettingTabConfig.map((tab) => (
+                <Tab.Panel key={tab.index}>
+                  <tab.component />
+                </Tab.Panel>
+              ))}
             </Tab.Panels>
           </Tab.Group>
         </div>
