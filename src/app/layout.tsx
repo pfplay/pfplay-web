111b5ad0c6b37b5a0cdae1c113be535367884669
Will Kim <will@chequer.io>
2023-08-04 02:25:43 +0900

    feat: 기존 나눔고딕 폰트 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import '@/styles/globals.css';
 import Header from '@/components/Header';
 import SessionProvider from '@/context/SessionProvider';
-import { fontNanumGothic } from '@/lib/fonts';
-import { cn } from '@/lib/utils';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <html lang='en'>
-      <body className={cn('min-h-screen font-sans', fontNanumGothic.className)}>
+      <body className='min-h-screen'>
         <SessionProvider>
           {/* TODO: Header를 react-router-dom의 useMatch hook을 이용하는 것 처럼 조건부 렌더 하는 방법 찾기.  */}
           <Header />
