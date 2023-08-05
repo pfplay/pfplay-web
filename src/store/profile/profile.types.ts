de23d8f842283d1b12430a9ddeb5692c76ac0b4c
Jongok Yoon <yoonjongok1209@gmail.com>
2023-08-05 16:07:25 +0200

    docs: ✏️ 질문 코멘트 작성

--- src/store/profile/profile.types.ts
+++ src/store/profile/profile.types.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-// TODO: Login 적용된 후 로그인 User, Profile, AvatarBody, AvatarFace 상세 따라서 수정
+// TODO: AvatarBody 상세 정해지면 수정
 export const avatarBodySchema = z.object({
   id: z.number(),
   type: z.enum(['basic', 'dj', 'room', 'ref']),
@@ -12,6 +12,7 @@ export const avatarBodySchema = z.object({
 // TODO: AvatarFace 상세 정해지면 수정
 export const avatarFaceSchema = avatarBodySchema;
 
+// 질문: Profile설정(nickName, introduction)과 Avatar 설정을 따로 나누는게 맞는지? 아니면 지금처럼 한번에 하는게 맞는지?
 export const profileSchema = z.object({
   nickName: z.string().optional(),
   introduction: z.string().optional(),
@@ -19,5 +20,6 @@ export const profileSchema = z.object({
   avatarFace: avatarFaceSchema.optional(),
 });
 
+// TODO: AvatarBody 설정 PR merge 되면 이 AvatarBody 타입 활용해 관련 response 타입 정의
 export type AvatarBody = z.infer<typeof avatarBodySchema>;
 export type Profile = z.infer<typeof profileSchema>;
