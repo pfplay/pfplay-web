e97750b769a26d7d6a22d8af6e9e41ec143f477d
Will Kim <will@chequer.io>
2023-08-10 04:34:23 +0900

    feat: flow 함수 비동기 처리 가능하도록 개선

--- src/utils/flow.test.ts
+++ src/utils/flow.test.ts
@@ -1,28 +1,41 @@
 import { flow } from '@/utils/flow';
 
-type AnyFunction = (...args: unknown[]) => void;
-
 describe('utils/flow', () => {
+  const wait = <T>(v: T) => {
+    return new Promise<T>((resolve) => {
+      setTimeout(() => {
+        resolve(v);
+      }, 50);
+    });
+  };
   const addOne = (x: number) => x + 1;
+  const addOneAsync = (x: number) => wait(x + 1);
   const multiplyByTwo = (x: number) => x * 2;
+  const multiplyByTwoAsync = (x: number) => wait(x * 2);
 
-  it('빈 입력을 올바르게 처리해야 한다.', () => {
-    const functions: AnyFunction[] = [];
-    const result = flow(functions)();
+  test('빈 입력을 올바르게 처리해야 한다.', async () => {
+    const result = await flow([])();
     expect(result).toBeUndefined();
   });
 
-  it('단일 함수를 올바르게 처리해야 한다.', () => {
+  test('단일 함수를 올바르게 처리해야 한다.', async () => {
     const functions = [addOne];
-    const result = flow(functions)(5);
+    const result = await flow(functions)(1);
 
-    expect(result).toBe(6);
+    expect(result).toBe(2);
   });
 
-  it('여러 함수를 올바르게 처리해야 한다.', () => {
-    const functions = [addOne, multiplyByTwo, addOne, multiplyByTwo, addOne];
-    const result = flow(functions)(5);
+  test('여러 함수를 올바르게 처리해야 한다.', async () => {
+    const functions = [addOne, addOne, multiplyByTwo, multiplyByTwo];
+    const result = await flow(functions)(1);
 
-    expect(result).toBe(27); // ((((5 + 1) * 2) + 1) * 2) + 1 = 27
+    expect(result).toBe(12);
+  });
+
+  test('비동기 함수가 포함되어도 올바르게 처리해야 한다.', async () => {
+    const functions = [addOne, addOneAsync, multiplyByTwo, multiplyByTwoAsync];
+    const result = await flow(functions)(1);
+
+    expect(result).toBe(12);
   });
 });
--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -1,11 +1,11 @@
-export function flow<T>(functions: ((...args: T[]) => T)[]) {
+export function flow<T>(functions: ((...args: T[]) => T | Promise<T>)[]) {
   const length = functions.length;
 
-  return (...args: T[]) => {
+  return async (...args: T[]) => {
     let index = 0;
-    let result: T = length ? functions[index](...args) : args[0];
+    let result: T = length ? await functions[index](...args) : args[0];
     while (++index < length) {
-      result = functions[index](result);
+      result = await functions[index](result);
     }
     return result;
   };
