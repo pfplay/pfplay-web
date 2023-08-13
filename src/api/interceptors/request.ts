abc656459f06116f68971edd8b941d83c8f042de
Will Kim <will@chequer.io>
2023-08-13 20:54:54 +0900

    chore:  리뷰 반영

--- src/api/interceptors/request.ts
+++ src/api/interceptors/request.ts
@@ -2,9 +2,9 @@ import { getSession } from 'next-auth/react';
 import { AxiosRequestHeaders, InternalAxiosRequestConfig } from 'axios';
 import { printRequestLog } from '@/utils/log';
 
-const unAuthRoutes = new Set<string>([]); // TODO: accessToken 필요 없는 api url 추가
+const publicRoutes = new Set<string>([]); // TODO: accessToken 필요 없는 api url 추가
 export async function setAccessToken(config: InternalAxiosRequestConfig) {
-  if (config.url && unAuthRoutes.has(config.url)) {
+  if (config.url && publicRoutes.has(config.url)) {
     return config;
   }
 
