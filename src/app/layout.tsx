922886223bd326ec7d73324afb3b52647c7aa139
Will Kim <will@chequer.io>
2023-08-15 02:27:51 +0900

    feat: next-font 의 localFont 사용하도록 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import '@/styles/globals.css';
 import SessionCheck from '@/app/sessionCheck';
 import Header from '@/components/Header';
 import SessionProvider from '@/context/SessionProvider';
+import { cn } from '@/lib/utils';
+import { pretendardVariable } from '@/styles/fonts';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <html lang='en'>
-      <body className='min-h-screen'>
+      <body className={cn('min-h-screen', pretendardVariable.className)}>
         <SessionProvider>
           <SessionCheck>
             {/* TODO: Header를 react-router-dom의 useMatch hook을 이용하는 것 처럼 조건부 렌더 하는 방법 찾기.  */}
