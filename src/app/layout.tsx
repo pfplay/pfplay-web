82caa56e012bd9a156df031a66aec8bad205cd07
Jongok Yoon <yoonjongok1209@gmail.com>
2023-08-20 16:54:32 +0200

    refactor: 💡 리팩토링

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,18 @@ import Header from '@/components/Header';
 import SessionProvider from '@/context/SessionProvider';
 import { cn } from '@/lib/utils';
 import { pretendardVariable } from '@/styles/fonts';
+import SessionCheck from './sessionCheck';
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <html lang='en'>
       <body className={cn('min-h-screen', pretendardVariable.className)}>
         <SessionProvider>
-          {/* <SessionCheck> */}
-          {/* TODO: Header를 react-router-dom의 useMatch hook을 이용하는 것 처럼 조건부 렌더 하는 방법 찾기.  */}
-          <Header />
-          {children}
-          {/* </SessionCheck> */}
+          <SessionCheck>
+            {/* TODO: Header를 react-router-dom의 useMatch hook을 이용하는 것 처럼 조건부 렌더 하는 방법 찾기.  */}
+            <Header />
+            {children}
+          </SessionCheck>
         </SessionProvider>
       </body>
     </html>
--- src/components/_features/Avatar/AvatarBody.tsx
+++ src/components/_features/Avatar/AvatarBody.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Image from 'next/image';
 import { avatarBodyMockArr } from '@/config/profile-body-mock';
 import { cn } from '@/lib/utils';
