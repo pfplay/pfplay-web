6c9604a21236e851d387ea12cc777423b605b46e
Will Kim <will@chequer.io>
2023-09-02 04:27:02 +0900

    refactor: 로딩 아이콘 아톰으로 분리 (애니메이션 적용으로 특별취급)

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,27 @@
+import React, { CSSProperties, FC } from 'react';
+
+interface LoadingProps {
+  size?: CSSProperties['width'];
+  color?: CSSProperties['color'];
+}
+
+const Loading: FC<LoadingProps> = ({ size = '1em', color = 'currentColor' }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={size}
+      height={size}
+      viewBox='0 0 24 24'
+      fill='none'
+      className='animate-loading'
+    >
+      <path
+        d='M15.1312 2.5C19.1204 3.81408 22 7.57075 22 12C22 17.5228 17.5228 22 12 22C7.57075 22 3.81408 19.1204 2.5 15.1312'
+        stroke={color}
+        strokeWidth={3}
+      />
+    </svg>
+  );
+};
+
+export default Loading;
