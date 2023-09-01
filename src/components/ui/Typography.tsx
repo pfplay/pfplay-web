66c835bc76cd8b5259281cbcc3a0854f79cd0b26
Will Kim <will@chequer.io>
2023-09-02 03:20:10 +0900

    refactor: Typography cva 제거

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,49 +1,24 @@
 'use client';
 import React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
-import { RecursiveExcludeNull } from '@/types';
 
-type TypographyVariantProps = RecursiveExcludeNull<VariantProps<typeof typographyVariants>>;
-export type TypographyType = NonNullable<TypographyVariantProps['type']>;
-export interface TypographyProps extends React.ComponentProps<'p'>, TypographyVariantProps {}
+export type TypographyType =
+  | 'title1'
+  | 'title2'
+  | 'body1'
+  | 'body2'
+  | 'body3'
+  | 'detail1'
+  | 'detail2'
+  | 'caption1'
+  | 'caption2';
+type TypographyOverflow = 'ellipsis' | 'break-words' | 'break-all' | 'break-normal';
 
-const typographyVariants = cva([], {
-  variants: {
-    type: {
-      title1: ['text-[28px] font-bold leading-[1.3]'],
-      title2: ['text-[24px] font-bold leading-[1.3]'],
-      body1: ['text-[20px] font-bold leading-[1.5]'],
-      body2: ['text-[18px] font-bold leading-[1.5]'],
-      body3: ['text-[16px] font-bold leading-[1.5]'],
-      detail1: ['text-[16px] font-normal leading-[1.5]'],
-      detail2: ['text-[14px] font-semibold leading-[1.5]'],
-      caption1: ['text-[14px] font-normal leading-[1.5]'],
-      caption2: ['text-[12px] font-normal leading-[1.5]'],
-    },
-    overflow: {
-      ellipsis: 'truncate',
-      'break-words': 'break-words',
-      'break-all': 'break-all',
-    },
-    inline: {
-      true: 'inline-block',
-    },
-  },
-});
-
-const elDict: Record<TypographyType, keyof Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'p'>> = {
-  title1: 'h1',
-  title2: 'h2',
-  body1: 'p',
-  body2: 'p',
-  body3: 'p',
-  detail1: 'p',
-  detail2: 'p',
-  caption1: 'p',
-  caption2: 'p',
-};
-const titleTypes: TypographyType[] = ['title1', 'title2'];
+interface TypographyProps extends React.ComponentProps<'p'> {
+  type?: TypographyType;
+  overflow?: TypographyOverflow;
+  inline?: boolean;
+}
 
 const Typography = ({
   className,
@@ -59,12 +34,49 @@ const Typography = ({
   return (
     <El
       title={title}
-      className={cn(typographyVariants({ type, overflow, className, inline }))}
+      className={cn([
+        typoStyleDict[type],
+        overflow && overflowDict[overflow],
+        inline && 'inline-block',
+
+        className,
+      ])}
       {...props}
     >
       {children}
     </El>
   );
+};
+
+const elDict: Record<TypographyType, keyof Pick<JSX.IntrinsicElements, 'h1' | 'h2' | 'p'>> = {
+  title1: 'h1',
+  title2: 'h2',
+  body1: 'p',
+  body2: 'p',
+  body3: 'p',
+  detail1: 'p',
+  detail2: 'p',
+  caption1: 'p',
+  caption2: 'p',
+};
+const titleTypes: TypographyType[] = ['title1', 'title2'];
+
+const typoStyleDict: Record<TypographyType, string> = {
+  title1: 'text-[28px] font-bold leading-[1.3]',
+  title2: 'text-[24px] font-bold leading-[1.3]',
+  body1: 'text-[20px] font-bold leading-[1.5]',
+  body2: 'text-[18px] font-bold leading-[1.5]',
+  body3: 'text-[16px] font-bold leading-[1.5]',
+  detail1: 'text-[16px] font-normal leading-[1.5]',
+  detail2: 'text-[14px] font-semibold leading-[1.5]',
+  caption1: 'text-[14px] font-normal leading-[1.5]',
+  caption2: 'text-[12px] font-normal leading-[1.5]',
+};
+const overflowDict: Record<TypographyOverflow, string> = {
+  ellipsis: 'ellipsis',
+  'break-words': 'break-words',
+  'break-all': 'break-all',
+  'break-normal': 'break-normal',
 };
 
 export default Typography;
