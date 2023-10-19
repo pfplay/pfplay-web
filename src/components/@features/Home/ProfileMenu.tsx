3e8f358e4e932d4746978ef6443c63a1c4d6191d
Jongok Yoon <yoonjongok1209@gmail.com>
2023-10-19 21:13:58 +0200

    refactor: 💡 passing email to ProfileMeun instead of whole sessi

--- src/components/@features/Home/ProfileMenu.tsx
+++ src/components/@features/Home/ProfileMenu.tsx
@@ -1,19 +1,18 @@
-import { Session } from 'next-auth';
 import { signOut } from 'next-auth/react';
 import { Menu } from '@headlessui/react';
 import MenuButton from '@/components/@shared/@atoms/Menu/MenuButton';
 import MenuItemPanel from '@/components/@shared/@atoms/Menu/MenuItemPanel';
 
 interface ProfileMenuProps {
-  userData: Session;
+  email: string;
 }
 
-const ProfileMenu = ({ userData }: ProfileMenuProps) => {
+const ProfileMenu = ({ email }: ProfileMenuProps) => {
   return (
     <Menu as='section' className={`relative w-fit`}>
       {({ close }) => (
         <>
-          <MenuButton type='button'>{userData.user.email}</MenuButton>
+          <MenuButton type='button'>{email}</MenuButton>
           <MenuItemPanel
             menuItemConfig={[{ label: '로그아웃', onClickItem: () => signOut() }]}
             close={close}
