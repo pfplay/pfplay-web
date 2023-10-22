09ac6d2f111b001fffd8327cc59927a843778590
Will Kim <will@chequer.io>
2023-10-22 18:40:12 +0900

    feat: copyJavaEnums 스크립트 작성 및 실행

--- src/api/@types/@enums.ts
+++ src/api/@types/@enums.ts
@@ -1,5 +1,14 @@
+export enum ApiHeader {
+  AUTHORIZATION = 'AUTHORIZATION',
+  BEARER = 'BEARER',
+}
+
 export enum Authority {
   ROLE_USER = 'ROLE_USER',
   ROLE_GUEST = 'ROLE_GUEST',
   ROLE_WALLET_USER = 'ROLE_WALLET_USER',
+}
+
+export enum Domain {
+  CLIENT = 'CLIENT',
 }
