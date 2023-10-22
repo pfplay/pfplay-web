79013e5b792651742e50cc5856a221a284e94d53
Will Kim <will@chequer.io>
2023-10-22 22:28:50 +0900

    feat: AvatarBodyList fetch (부모에서 받아서 내려주는 방식으로 변경)

--- src/components/features/Avatar/AvatarSettingForm.tsx
+++ src/components/features/Avatar/AvatarSettingForm.tsx
@@ -1,70 +1,93 @@
 'use client';
 import Image from 'next/image';
-import React, { FC, Fragment, PropsWithChildren, useState } from 'react';
+import React, { FC, Fragment, PropsWithChildren, useEffect, useState } from 'react';
 import { Tab } from '@headlessui/react';
+import { AvatarParts } from '@/api/@types/Avatar';
+import { AvatarService } from '@/api/services/Avatar';
+import AvatarBodyList from '@/components/features/Avatar/AvatarBodyList';
+import AvatarFaceList from '@/components/features/Avatar/AvatarFaceList';
 import BackButton from '@/components/shared/BackButton';
 import CustomLink from '@/components/shared/CustomLink';
 import CustomTab from '@/components/shared/atoms/CustomTab';
-import { mockAvatarBodyList } from '@/constants/__mock__/mockAvatarBodyList';
+import { mockAvatarPartsList } from '@/constants/__mock__/mockAvatarPartsList';
+import { useDialog } from '@/hooks/useDialog';
 import { cn } from '@/utils/cn';
 import { ROUTES } from '@/utils/routes';
-import AvatarBody from './AvatarBody';
-import AvatarFace from './AvatarFace';
-import { AvatarImg } from './AvatarImage';
 
 interface Props {
   withLayout?: boolean;
 }
 
-const avatarSettingTabConfig: Array<{ name: 'body' | 'face'; index: number }> = [
-  { name: 'body', index: 0 },
-  { name: 'face', index: 1 },
-];
-
-const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div className='absolute-user-form-section'>
-      <BackButton text='뭘 입고 놀아볼까요?' className='absolute top-10 left-[60px]' />
-      <div className='h-full flexCol justify-start max-w-screen-desktop mx-auto gap-10 pt-[112px] pb-10 px-[60px]'>
-        {children}
-      </div>
-    </div>
-  );
-};
-
 const AvatarSettingForm: FC<Props> = ({ withLayout }) => {
-  const [selectedBody, setSelectedBody] = useState<AvatarImg>(mockAvatarBodyList[0]);
-  const [selectedFace, setSelectedFace] = useState<AvatarImg>(mockAvatarBodyList[0]); // FIXME: Avatar face type 정해면 재정의
-
+  const { openErrorDialog } = useDialog();
+  const [bodyList, setBodyList] = useState<AvatarParts[]>([]);
+  const [faceList, setFaceList] = useState<AvatarParts[]>([]);
+  const [selectedBody, setSelectedBody] = useState<AvatarParts>();
+  const [selectedFace, setSelectedFace] = useState<AvatarParts>();
   const Container = withLayout ? Layout : Fragment;
+
+  const fetchBodyList = async () => {
+    try {
+      const bodyList = await AvatarService.getBodyList();
+      setBodyList(bodyList);
+      setSelectedBody(bodyList[0]);
+    } catch (e) {
+      openErrorDialog(e);
+    }
+  };
+
+  const fetchFaceList = async () => {
+    try {
+      // TODO: fetch face list
+      setFaceList(mockAvatarPartsList);
+      setSelectedFace(mockAvatarPartsList[0]);
+    } catch (e) {
+      openErrorDialog(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchBodyList();
+    fetchFaceList();
+  }, []);
 
   return (
     <Container>
       <div className='flexRow gap-[60px]'>
         <div className='items-center h-full bg-black pointer-events-none select-none flexRow'>
-          <Image
-            src={selectedBody.image}
-            alt={selectedBody.name}
-            width={300}
-            height={300}
-            sizes='(max-width:300px)'
-            className='bg-black min-w-[300px]'
-          />
+          {!(selectedBody && selectedFace) && <div className='bg-black w-[300px] h-[300px]' />}
+          {selectedBody && selectedFace && (
+            <Image
+              src={selectedBody.image}
+              alt={selectedBody.name}
+              width={300}
+              height={300}
+              sizes='(max-width:300px)'
+              className='bg-black min-w-[300px]'
+            />
+          )}
         </div>
         <div className='w-full flexCol'>
           <Tab.Group>
             <Tab.List className={cn('w-full flexRow')}>
-              {avatarSettingTabConfig.map((tab) => (
-                <CustomTab key={tab.index} tabTitle={tab.name} variant='line' />
-              ))}
+              <CustomTab tabTitle='body' variant='line' />
+              <CustomTab tabTitle='face' variant='line' />
               <div className='flex-1 border-b-[1px] border-b-gray-400' />
             </Tab.List>
             <Tab.Panels>
-              <Tab.Panel tabIndex={avatarSettingTabConfig[0].index}>
-                <AvatarBody selectedBody={selectedBody} setSelectedBody={setSelectedBody} />
+              <Tab.Panel tabIndex={0}>
+                <AvatarBodyList
+                  list={bodyList}
+                  selected={selectedBody}
+                  setSelected={setSelectedBody}
+                />
               </Tab.Panel>
-              <Tab.Panel tabIndex={avatarSettingTabConfig[1].index}>
-                <AvatarFace selectedFace={selectedFace} setSelectedFace={setSelectedFace} />
+              <Tab.Panel tabIndex={1}>
+                <AvatarFaceList
+                  list={faceList}
+                  selected={selectedFace}
+                  setSelected={setSelectedFace}
+                />
               </Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
@@ -83,5 +106,16 @@ const AvatarSettingForm: FC<Props> = ({ withLayout }) => {
     </Container>
   );
 };
+
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className='absolute-user-form-section'>
+      <BackButton text='뭘 입고 놀아볼까요?' className='absolute top-10 left-[60px]' />
+      <div className='h-full flexCol justify-start max-w-screen-desktop mx-auto gap-10 pt-[112px] pb-10 px-[60px]'>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default AvatarSettingForm;
