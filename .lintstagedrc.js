550f1481dfce3d9e866bec583175d9622e5d91fe
Jongok Yoon <yoonjongok1209@gmail.com>
2023-11-13 21:11:34 +0100

    fix: 🐛 Reset lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 const buildLintCommand = (filenames) =>
-  `yarn next lint --fix --file ${filenames
+  `yarn lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
 const buildPrettierCommand = (filenames) =>
-  `yarn prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+  `yarn format --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 module.exports = {
   '*.{ts,tsx}': [buildPrettierCommand, buildLintCommand],
