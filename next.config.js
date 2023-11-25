61522de1ed52af4c757ce65ba95d263537c26bff
Jongok Yoon <yoonjongok1209@gmail.com>
2023-11-25 18:50:23 +0100

    fix: 🐛 build fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
