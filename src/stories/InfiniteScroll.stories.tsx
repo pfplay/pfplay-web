30183d087b2858b7cadbb11d0ba6065096ad6293
Will Kim <will@chequer.io>
2023-12-17 19:11:59 +0900

    feat: InfiniteScroll 컴포넌트 작성

---
+++ src/stories/InfiniteScroll.stories.tsx
@@ -0,0 +1,43 @@
+// InfiniteScroll.stories.tsx
+import { useState } from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import InfiniteScroll from '@/components/shared/InfiniteScroll';
+
+const meta: Meta<typeof InfiniteScroll> = {
+  title: 'ui/InfiniteScroll',
+  component: InfiniteScroll,
+};
+
+export default meta;
+
+const mockData = new Array(50).fill(null).map((_, index) => `Item ${index + 1}`);
+export const Preview: StoryFn<typeof meta> = () => {
+  const [data, setData] = useState(mockData.slice(0, 10));
+  const [hasMore, setHasMore] = useState(true);
+  const [loadCount, setLoadCount] = useState(1);
+
+  const loadMore = () => {
+    if (loadCount >= 3) {
+      setHasMore(false);
+      return;
+    }
+
+    setTimeout(() => {
+      setData((currentData) => [
+        ...currentData,
+        ...mockData.slice(loadCount * 10, (loadCount + 1) * 10),
+      ]);
+      setLoadCount(loadCount + 1);
+    }, 1000); // Simulate network request delay
+  };
+
+  return (
+    <InfiniteScroll load={loadMore} hasMore={hasMore} className='h-[600px]'>
+      {data.map((item) => (
+        <div key={item} className='h-[100px]'>
+          {item}
+        </div>
+      ))}
+    </InfiniteScroll>
+  );
+};
