9e2c535bb638cd57725ad7835310e4e222f6e5c1
Will Kim <will@chequer.io>
2023-12-17 19:11:59 +0900

    feat: InfiniteScroll 컴포넌트 작성

---
+++ src/components/shared/InfiniteScroll.tsx
@@ -0,0 +1,69 @@
+import { FC, ReactNode, useCallback, useEffect, useRef } from 'react';
+import Loading from '@/components/shared/atoms/Loading';
+import { cn } from '@/utils/cn';
+
+interface Props {
+  load: () => void;
+  hasMore: boolean;
+  endMessage?: ReactNode;
+  children?: ReactNode;
+  containerClassName?: string;
+}
+
+const InfiniteScroll: FC<Props> = ({
+  children,
+  load,
+  hasMore,
+  endMessage = 'No more data',
+  containerClassName,
+}) => {
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  const observedRef = useRef<HTMLDivElement | null>(null);
+
+  const handleIntersect: IntersectionObserverCallback = useCallback(
+    (entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        load();
+      }
+    },
+    [load, hasMore]
+  );
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver(handleIntersect, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.8,
+    });
+
+    if (observedRef.current) {
+      observerRef.current.observe(observedRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, [handleIntersect]);
+
+  useEffect(() => {
+    if (observerRef.current && observedRef.current) {
+      observerRef.current.disconnect();
+      observerRef.current.observe(observedRef.current);
+    }
+  }, [hasMore]);
+
+  return (
+    <div className={containerClassName}>
+      {children}
+      {(hasMore || endMessage) && (
+        <div ref={observedRef} className={cn('h-[300px] flexRowCenter text-[20px]')}>
+          {hasMore ? <Loading /> : endMessage}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
