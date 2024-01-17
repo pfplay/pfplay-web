6fe1e9f6d17f5995eba89a7e8ac4e71a91e7c3e4
jh.park <jh.park@ent-bc.com>
2024-01-17 14:35:55 +0900

    feat: redirect

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,8 +1,15 @@
+import { redirect } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 import Header from '@/components/layouts/Header';
 import WalletProvider from '@/context/WalletProvider';
+import { getServerAuthSession } from '@/utils/authOptions';
 
-const ProfileEditLayout = ({ children }: PropsWithChildren) => {
+const ProfileEditLayout = async ({ children }: PropsWithChildren) => {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    redirect('/');
+  }
   return (
     <WalletProvider>
       <Header />
