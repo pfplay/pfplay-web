f34acc767422da09c8ec5aa25e6cf8436d5096b8
jh.park <jh.park@ent-bc.com>
2024-01-18 16:26:33 +0900

    fix: query key

--- src/api/query-temp/playlist/useYoutubeInfiniteQuery.ts
+++ src/api/query-temp/playlist/useYoutubeInfiniteQuery.ts
@@ -3,7 +3,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { PlaylistService } from '@/api/services/Playlist';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 
-const YOUTUBE_INFINITE_QUERY_KEY = ['YOUTUBE'];
+const YOUTUBE_INFINITE_QUERY_KEY = 'YOUTUBE';
 const useYoutubeInfiniteQuery = (q: string = '') => {
   const { setRef, isIntersecting } = useIntersectionObserver();
   const { fetchNextPage, ...query } = useInfiniteQuery({
