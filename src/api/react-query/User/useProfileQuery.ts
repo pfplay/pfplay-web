e2a345200b51fef0512d73e2841b8955625b6bec
Will Kim <will@chequer.io>
2024-02-11 23:13:54 +0900

    refactor: user - query key 상수화, useInvalidate...Query 를 내보내는 패턴 제거

--- src/api/react-query/User/useProfileQuery.ts
+++ src/api/react-query/User/useProfileQuery.ts
@@ -1,20 +1,16 @@
 import { useSession } from 'next-auth/react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { UserService } from '@/api/services/User';
+import { USER_PROFILE_QUERY_KEY } from './keys';
 
-export const PROFILE_QUERY_KEY = 'PROFILE';
 export const useProfileQuery = () => {
   const session = useSession();
+
   return useQuery({
-    queryKey: [PROFILE_QUERY_KEY],
+    queryKey: [USER_PROFILE_QUERY_KEY],
     queryFn: () => UserService.getProfile(),
     gcTime: Infinity,
     staleTime: Infinity,
     enabled: !!session,
   });
-};
-
-export const useInvalidateProfileQuery = () => {
-  const queryClient = useQueryClient();
-  return () => queryClient.invalidateQueries({ queryKey: [PROFILE_QUERY_KEY] });
 };
--- src/api/react-query/User/useProfileUpdateMutation.ts
+++ src/api/react-query/User/useProfileUpdateMutation.ts
@@ -1,22 +1,20 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { UserProfile } from '@/api/@types/User';
-import {
-  PROFILE_QUERY_KEY,
-  useInvalidateProfileQuery,
-} from '@/api/react-query/User/useProfileQuery';
+import { USER_PROFILE_QUERY_KEY } from '@/api/react-query/User/keys';
 import { UserService } from '@/api/services/User';
 
 export const useProfileUpdateMutation = () => {
   const queryClient = useQueryClient();
-  const invalidateProfileQuery = useInvalidateProfileQuery();
 
   return useMutation({
     mutationFn: (data: UserProfile) => {
-      const prev = queryClient.getQueryData<UserProfile>([PROFILE_QUERY_KEY]);
+      const prev = queryClient.getQueryData<UserProfile>([USER_PROFILE_QUERY_KEY]);
       return UserService.updateProfile({ ...prev, ...data });
     },
     onSuccess: () => {
-      invalidateProfileQuery();
+      queryClient.invalidateQueries({
+        queryKey: [USER_PROFILE_QUERY_KEY],
+      });
     },
   });
 };
