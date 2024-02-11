ecd65e9700c9fa2659f7a5c4dc820abcfa006b1a
Will Kim <will@chequer.io>
2024-02-11 23:17:28 +0900

    refactor: user - staleTime, gcTime 수정

--- src/api/react-query/User/useProfileQuery.ts
+++ src/api/react-query/User/useProfileQuery.ts
@@ -1,6 +1,7 @@
 import { useSession } from 'next-auth/react';
 import { useQuery } from '@tanstack/react-query';
 import { UserService } from '@/api/services/User';
+import { FIVE_MINUTES } from '@/constants/time';
 import { USER_PROFILE_QUERY_KEY } from './keys';
 
 export const useProfileQuery = () => {
@@ -9,8 +10,8 @@ export const useProfileQuery = () => {
   return useQuery({
     queryKey: [USER_PROFILE_QUERY_KEY],
     queryFn: () => UserService.getProfile(),
+    staleTime: FIVE_MINUTES,
     gcTime: Infinity,
-    staleTime: Infinity,
     enabled: !!session,
   });
 };
