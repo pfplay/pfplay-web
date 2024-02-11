5e1a2b7d79fbcf4f0e2748cc2d15f67d04c29763
Will Kim <will@chequer.io>
2024-02-11 17:33:43 +0900

    chore: 'React.' 제거, config 컴포넌트 외부로 분리

--- src/context/ReactQueryProvider.tsx
+++ src/context/ReactQueryProvider.tsx
@@ -1,26 +1,26 @@
 'use client';
 
-import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren, useRef } from 'react';
+import { QueryClient, QueryClientConfig, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-export function ReactQueryProvider({ children }: React.PropsWithChildren) {
-  const [client] = React.useState(
-    new QueryClient({
-      defaultOptions: {
-        queries: {
-          /**
-           * @see https://tanstack.com/query/latest/docs/react/guides/advanced-ssr
-           *
-           * With SSR, we usually want to set some default staleTime
-           * above 0 to avoid refetching immediately on the client
-           *
-           */
-          staleTime: 60 * 1000,
-        },
-      },
-    })
-  );
+const config: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      /**
+       * @see https://tanstack.com/query/latest/docs/react/guides/advanced-ssr
+       *
+       * With SSR, we usually want to set some default staleTime
+       * above 0 to avoid refetching immediately on the client
+       *
+       */
+      staleTime: 60 * 1000,
+    },
+  },
+};
+
+export function ReactQueryProvider({ children }: PropsWithChildren) {
+  const client = useRef(new QueryClient(config)).current;
 
   return (
     <QueryClientProvider client={client}>
