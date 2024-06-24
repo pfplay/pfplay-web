f2982841829f216839d2a123fb14c566f48abe40
Will Kim <will@chequer.io>
2024-06-25 03:30:30 +0900

    fix: useSuspenseQuery가 nextjs app router 에서 SSR 시 무한 refetch를 트리거하는 이슈 (#123)
    fix: useSuspenseQuery가 nextjs app router 에서 SSR 시 무한 refetch를 트리거하는 이슈 수정

--- src/app/_providers/react-query.provider.tsx
+++ src/app/_providers/react-query.provider.tsx
@@ -1,42 +1,60 @@
 'use client';
-
-import { PropsWithChildren, useState } from 'react';
+import { ReactNode } from 'react';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { getErrorMessage } from '@/shared/api/get-error-message';
 import isAuthError from '@/shared/api/is-auth-error';
 import { FIVE_MINUTES } from '@/shared/config/time';
 
-export function ReactQueryProvider({ children }: PropsWithChildren) {
-  const [client] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            /**
-             * @see https://tanstack.com/query/latest/docs/react/guides/advanced-ssr
-             *
-             * With SSR, we usually want to set some default staleTime
-             * above 0 to avoid refetching immediately on the client
-             *
-             */
-            staleTime: FIVE_MINUTES,
-            refetchOnWindowFocus: false,
-            retry: process.env.NODE_ENV === 'development' ? false : 3,
-          },
-        },
-        queryCache: new QueryCache({
-          onError: handleBubbledError,
-        }),
-      })
-  );
+/**
+ * @see https://github.com/TanStack/query/issues/6116#issuecomment-1904051005
+ */
+export default function ReactQueryProvider({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>
+        {children}
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+      </ReactQueryStreamedHydration>
     </QueryClientProvider>
   );
+}
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        /**
+         * @see https://tanstack.com/query/latest/docs/react/guides/advanced-ssr
+         *
+         * With SSR, we usually want to set some default staleTime
+         * above 0 to avoid refetching immediately on the client
+         *
+         */
+        staleTime: FIVE_MINUTES,
+        refetchOnWindowFocus: false,
+        retry: process.env.NODE_ENV === 'development' ? false : 3,
+      },
+    },
+    queryCache: new QueryCache({
+      onError: handleBubbledError,
+    }),
+  });
+}
+
+let clientQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    // Server: always make a new query client
+    return makeQueryClient();
+  }
+  // Browser: make a new query client if we don't already have one
+  if (!clientQueryClient) clientQueryClient = makeQueryClient();
+  return clientQueryClient;
 }
 
 /**
