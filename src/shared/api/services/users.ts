2482b4ac52cf56ee05e0ad8370ff8c277e9a19f8
Will Kim <will@chequer.io>
2024-06-24 22:25:22 +0900

    fix: 라우트 수정

--- src/shared/api/services/users.ts
+++ src/shared/api/services/users.ts
@@ -11,10 +11,10 @@ export const UsersService: UsersClient = {
     return pfpAxiosInstance.post(`${ROUTE_V1}/logout`);
   },
   temporary_SignInFullMember: () => {
-    return pfpAxiosInstance.post(`${ROUTE_V1}/sign/temporary/full-member`);
+    return pfpAxiosInstance.post(`${ROUTE_V1}/members/sign/temporary/full-member`);
   },
   temporary_SignInAssociateMember: () => {
-    return pfpAxiosInstance.post(`${ROUTE_V1}/sign/temporary/associate-member`);
+    return pfpAxiosInstance.post(`${ROUTE_V1}/members/sign/temporary/associate-member`);
   },
   getMyInfo: () => {
     return pfpAxiosInstance.get(`${ROUTE_V1}/me/info`);
