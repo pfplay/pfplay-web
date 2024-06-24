999cf5b5601510923faed98d8e2e1bab867005bf
Will Kim <will@chequer.io>
2024-06-25 03:19:14 +0900

    chore: improve global error boundary page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useEffect } from 'react';
+import { getErrorMessage } from '@/shared/api/get-error-message';
 import isAuthError from '@/shared/api/is-auth-error';
+import { cn } from '@/shared/lib/functions/cn';
 import { Button } from '@/shared/ui/components/button';
 import { Typography } from '@/shared/ui/components/typography';
 
@@ -13,11 +15,24 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   }, [error]);
 
   return (
-    <div className='h-screen flexColCenter gap-[10px]'>
+    <div className='h-screen flexColCenter gap-[10px] p-[20px]'>
       <Typography type='title2'>Something went wrong!</Typography>
       <Button variant='fill' color='primary' onClick={() => reset()}>
         Try again
       </Button>
+
+      {process.env.NODE_ENV === 'development' && (
+        <Typography
+          type='caption1'
+          className={cn(
+            'relative w-[400px] max-w-full bg-gray-700 text-gray-200 p-[20px] rounded-b mt-[20px]',
+            'before:content-["Error_Message"] before:absolute before:z-1',
+            'before:-top-[20px] before:left-0'
+          )}
+        >
+          {getErrorMessage(error)}
+        </Typography>
+      )}
     </div>
   );
 }
