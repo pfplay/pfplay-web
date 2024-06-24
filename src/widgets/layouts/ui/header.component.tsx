ef674d0257063c90b3a8e7278f23d39d41293716
Will Kim <will@chequer.io>
2024-06-25 01:01:50 +0900

    fix: 잘못 들어간 링크 제거

--- src/widgets/layouts/ui/header.component.tsx
+++ src/widgets/layouts/ui/header.component.tsx
@@ -34,7 +34,7 @@ const Header: FC<Props> = ({ withLogo }) => {
         )}
       >
         {withLogo && (
-          <Link href='/public'>
+          <Link href='/'>
             <Image
               src='/images/Logo/wordmark_small_white.png'
               width={124}
