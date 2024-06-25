935cd9101fc1cee8ff981927c058eb25a234b34e
Will Kim <will@chequer.io>
2024-06-26 02:16:38 +0900

    fix: type error

--- src/shared/api/__fixture__/playlist-musics.fixture.ts
+++ src/shared/api/__fixture__/playlist-musics.fixture.ts
@@ -1,14 +1,16 @@
 import { PlaylistMusic } from '@/shared/api/types/playlists';
 
-export const fixturePlaylistMusics: Omit<PlaylistMusic, 'ownerId' | 'orderNumber'>[] = [
+export const fixturePlaylistMusics: PlaylistMusic[] = [
   {
     musicId: 1,
+    orderNumber: 1,
     name: 'BLACKPINK(블랙핑크) - Shut Down @인기가요sssss inkigayo 20220925 long long long long long long long longlong long long long text',
     duration: '00:00',
     thumbnailImage: '/images/Background/Profile.png',
   },
   {
     musicId: 2,
+    orderNumber: 2,
     name: 'BLACKPINK(블랙핑크)checkehck ',
     duration: '04:20',
     thumbnailImage: '/images/Background/Profile.png',
