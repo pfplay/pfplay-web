22ecfe73f95c412a4d0d7ac777dcab1bb3c73cfc
Will Kim <will@chequer.io>
2024-06-30 01:04:25 +0900

    chore: useVerticalStretch 훅 공용으로 분리

--- src/shared/ui/components/loading/loading-panel.component.tsx
+++ src/shared/ui/components/loading/loading-panel.component.tsx
@@ -1,12 +1,7 @@
 'use client';
-import { useLayoutEffect, useState } from 'react';
+import { useVerticalStretch } from '@/shared/lib/hooks/use-vertical-stretch.hook';
 import Loading, { type LoadingProps } from './loading.component';
 
-/**
- * 높이는 부모 높이를 따라갑니다.
- * - 부모가 flex column일 경우 - flex-grow: 1
- * - 부모가 flex column이 아닐 경우 - height: 100%
- */
 export default function LoadingPanel({ size = 24, ...rest }: LoadingProps) {
   const setRef = useVerticalStretch<HTMLDivElement>();
 
@@ -15,21 +10,4 @@ export default function LoadingPanel({ size = 24, ...rest }: LoadingProps) {
       <Loading size={size} {...rest} />
     </div>
   );
-}
-
-function useVerticalStretch<EL extends HTMLElement>() {
-  const [ref, setRef] = useState<EL | null>(null);
-
-  useLayoutEffect(() => {
-    if (ref && ref.parentElement) {
-      const parentStyle = window.getComputedStyle(ref.parentElement);
-      if (parentStyle.display === 'flex' && parentStyle.flexDirection === 'column') {
-        ref.style.flex = '1';
-      } else {
-        ref.style.height = '100%';
-      }
-    }
-  }, [ref]);
-
-  return setRef;
 }
