8bd4f6ed9e53878c422c3b276a6404c0b5ddde28
Will Kim <will@chequer.io>
2024-06-29 16:36:21 +0900

    feat(app/sign-in): 로그인 페이지에 작성한 feature 적용, 둘러보기 버튼 임시 주석처리

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,87 +1,45 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { UsersService } from '@/shared/api/services/users';
+import { SignInByGoogleButtonForDev, useSignInByGoogle } from '@/features/sign-in/by-google';
 import { useI18n } from '@/shared/lib/localization/i18n.context';
-import { useAppRouter } from '@/shared/lib/router/use-app-router.hook';
 import { Button } from '@/shared/ui/components/button';
-import { Dialog } from '@/shared/ui/components/dialog';
-import { useDialog } from '@/shared/ui/components/dialog';
-import { TextButton } from '@/shared/ui/components/text-button';
-import { Typography } from '@/shared/ui/components/typography';
 import { PFClose } from '@/shared/ui/icons';
 
 const SignInPage = () => {
   const t = useI18n();
-  const router = useAppRouter();
-  const { openDialog } = useDialog();
+  const signInByGoogle = useSignInByGoogle();
 
-  const signInGoogle = async () => {
-    // FIXME: API 개발 전 임시 로직
-    return openDialog((_, onClose) => ({
-      title: t.onboard.title.moment,
-      Sub: (
-        <Typography type='detail1' className='text-gray-300'>
-          개발용 로그인입니다. 로그인할 권한을 선택하세요.
-        </Typography>
-      ),
-      Body: () => {
-        return (
-          <Dialog.ButtonGroup>
-            <Dialog.Button
-              color='secondary'
-              onClick={async () => {
-                await UsersService.temporary_SignInFullMember();
-                onClose?.();
-                router.push('/parties');
-              }}
-            >
-              Full
-            </Dialog.Button>
-            <Dialog.Button
-              onClick={async () => {
-                await UsersService.temporary_SignInAssociateMember();
-                onClose?.();
-                router.push('/parties');
-              }}
-            >
-              Associate
-            </Dialog.Button>
-          </Dialog.ButtonGroup>
-        );
-      },
-    }));
-  };
-
-  const openLookAroundDialog = () => {
-    return openDialog<number>((_, onClose) => ({
-      title: t.onboard.title.moment,
-      Sub: (
-        <Typography type='detail1' className='text-gray-300'>
-          {t.onboard.para.guest_limit}
-        </Typography>
-      ),
-      Body: () => {
-        return (
-          <Dialog.ButtonGroup>
-            <Dialog.Button
-              color='secondary'
-              onClick={() => {
-                onClose?.();
-                // 비로그인 유저가 로그인 없이 서비스를 이용할 때에는, 메인스테이지(파티룸)로 바로 뛀궈준다.
-                // FIXME: 게스트 로그인 API 호출
-                router.push('/parties/[id]', { path: { id: 1 } });
-              }}
-              className='flex-none px-[10.5px]'
-            >
-              {t.onboard.btn.guest}
-            </Dialog.Button>
-            <Dialog.Button onClick={signInGoogle}>{t.auth.btn.connect_google}</Dialog.Button>
-          </Dialog.ButtonGroup>
-        );
-      },
-    }));
-  };
+  // TODO: Onchain 빌더톤 끝나면 주석 해제
+  // const openLookAroundDialog = () => {
+  //   return openDialog<number>((_, onClose) => ({
+  //     title: t.onboard.title.moment,
+  //     Sub: (
+  //       <Typography type='detail1' className='text-gray-300'>
+  //         {t.onboard.para.guest_limit}
+  //       </Typography>
+  //     ),
+  //     Body: () => {
+  //       return (
+  //         <Dialog.ButtonGroup>
+  //           <Dialog.Button
+  //             color='secondary'
+  //             onClick={() => {
+  //               onClose?.();
+  //               // 비로그인 유저가 로그인 없이 서비스를 이용할 때에는, 메인스테이지(파티룸)로 바로 뛀궈준다.
+  //               // FIXME: 게스트 로그인 API 호출
+  //               router.push('/parties/[id]', { path: { id: 1 } });
+  //             }}
+  //             className='flex-none px-[10.5px]'
+  //           >
+  //             {t.onboard.btn.guest}
+  //           </Dialog.Button>
+  //           <Dialog.Button onClick={signInGoogle}>{t.auth.btn.connect_google}</Dialog.Button>
+  //         </Dialog.ButtonGroup>
+  //       );
+  //     },
+  //   }));
+  // };
 
   return (
     <div className='relative min-h-screen flexRowCenter'>
@@ -100,21 +58,23 @@ const SignInPage = () => {
           />
         </div>
 
+        <SignInByGoogleButtonForDev />
+
         <Button
           size='xl'
           typo='detail1'
           color='secondary'
           variant='outline'
           Icon={<Image src='/images/ETC/google.png' alt='google' width={32} height={32} />}
-          onClick={signInGoogle}
+          onClick={signInByGoogle}
           className='w-[320px] h-[56px] mb-[40px]'
         >
           {t.auth.btn.connect_google}
         </Button>
 
-        <TextButton onClick={openLookAroundDialog} underline>
-          {t.onboard.btn.take_a_look}
-        </TextButton>
+        {/*<TextButton onClick={openLookAroundDialog} underline>*/}
+        {/*  {t.onboard.btn.take_a_look}*/}
+        {/*</TextButton>*/}
       </div>
     </div>
   );
