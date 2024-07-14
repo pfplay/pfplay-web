3ddb38feda1a541a6d5b0cef8f2d92268a46bcc2
Will Kim <will@chequer.io>
2024-07-14 18:19:25 +0900

    feat(shared/lib): mock decorator 및 관련 환경변수 추가

---
+++ src/shared/lib/decorators/mock/index.ts
@@ -0,0 +1,2 @@
+export { default as MockResolve } from 'shared/lib/decorators/mock/mock-resolve.decorator';
+export { default as MockReturn } from 'shared/lib/decorators/mock/mock-return.decorator';
--- types/global.d.ts
+++ types/global.d.ts
@@ -6,6 +6,7 @@ declare global {
       NEXT_PUBLIC_API_WS_HOST_NAME: string;
       NEXT_PUBLIC_ALCHEMY_PUBLIC_API_KEY: string;
       NEXT_PUBLIC_WAGMI_PROJECT_ID: string;
+      NEXT_PUBLIC_USE_MOCK?: 'true' | 'false';
 
       // secret
       GOOGLE_ID: string;
