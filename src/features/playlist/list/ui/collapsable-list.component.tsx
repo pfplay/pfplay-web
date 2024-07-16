e96cd7a3f763d0d79bc805b335e24e036950553b
Jongok Yoon <yoonjongok1209@gmail.com>
2024-07-16 13:20:47 +0200

    refactor: move disclosurePanel to collapse list

---
+++ src/features/playlist/list/ui/collapsable-list.component.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { ReactElement } from 'react';
+import { usePlaylistAction } from '@/entities/playlist';
+import { Playlist } from '@/shared/api/http/types/playlists';
+import { useI18n } from '@/shared/lib/localization/i18n.context';
+import { replaceVar } from '@/shared/lib/localization/split-render';
+import { CollapseList } from '@/shared/ui/components/collapse-list';
+
+type CollapsibleListProps = {
+  musicsRender: (playlist: Playlist) => ReactElement;
+};
+
+const CollapsibleList = ({ musicsRender }: CollapsibleListProps) => {
+  const t = useI18n();
+  const playlistAction = usePlaylistAction();
+
+  return (
+    <div className='flexCol gap-3'>
+      {playlistAction.list.map((playlist) => (
+        <CollapseList
+          key={playlist.id}
+          title={playlist.name}
+          infoText={replaceVar(t.playlist.title.n_song, { $1: playlist.musicCount })}
+          classNames={{
+            panel: 'text-gray-200',
+          }}
+        >
+          {musicsRender(playlist)}
+        </CollapseList>
+      ))}
+    </div>
+  );
+};
+
+export default CollapsibleList;
