9d60a2c782d8aa617edc3a8141b67865a55cfe4d
Will Kim <will@chequer.io>
2024-07-16 23:24:48 +0900

    fix(shared/api): http service들 this 바인딩 문제 수정 (#137)
    - `queryFn: MyService.myMethod` 와 같이 사용할 경우 this 바인딩이 의도와 다르게 동작하는 문제를 수정하기 위해 서비스 메서드를 인스턴스 메서드로 변경

--- src/shared/api/http/services/parties.ts
+++ src/shared/api/http/services/parties.ts
@@ -12,15 +12,15 @@ import type {
 class PartiesService extends HTTPClient implements PartiesClient {
   private ROUTE_V1 = 'v1/party-room';
 
-  public create(request: CreatePartyroomRequest) {
+  public create = (request: CreatePartyroomRequest) => {
     return this.post<CreatePartyroomResponse>(`${this.ROUTE_V1}/create`, request);
-  }
+  };
 
-  public getList(request: PaginationPayload) {
+  public getList = (request: PaginationPayload) => {
     return this.get<PaginationResponse<PartyroomSummary>>(`${this.ROUTE_V1}/list`, {
       params: request,
     });
-  }
+  };
 }
 
 const instance = new PartiesService();
--- src/shared/api/http/services/users.ts
+++ src/shared/api/http/services/users.ts
@@ -20,68 +20,68 @@ import type {
 class UsersService extends HTTPClient implements UsersClient {
   private ROUTE_V1 = 'v1/users';
 
-  public signIn(request: SignInRequest) {
+  public signIn = (request: SignInRequest) => {
     if (typeof window === 'undefined') return;
 
     const url = new URL(`${this.axiosInstance.defaults.baseURL}${this.ROUTE_V1}/members/sign`);
     url.searchParams.append('oauth2Provider', request.oauth2Provider);
 
     window.location.href = url.toString();
-  }
+  };
 
-  public signInGuest(request: SignInGuestRequest) {
+  public signInGuest = (request: SignInGuestRequest) => {
     return this.post<void>(`${this.ROUTE_V1}/guests/sign`, request);
-  }
+  };
 
-  public signOut() {
+  public signOut = () => {
     return this.post<void>(`${this.ROUTE_V1}/logout`);
-  }
+  };
 
-  public temporary_SignInFullMember() {
+  public temporary_SignInFullMember = () => {
     return this.post<void>(`${this.ROUTE_V1}/members/sign/temporary/full-member`);
-  }
+  };
 
-  public temporary_SignInAssociateMember() {
+  public temporary_SignInAssociateMember = () => {
     return this.post<void>(`${this.ROUTE_V1}/members/sign/temporary/associate-member`);
-  }
+  };
 
-  public getMyInfo() {
+  public getMyInfo = () => {
     return this.get<GetMyInfoResponse>(`${this.ROUTE_V1}/me/info`);
-  }
+  };
 
-  public getUserProfileSummary(request: GetUserProfileSummaryRequest) {
+  public getUserProfileSummary = (request: GetUserProfileSummaryRequest) => {
     return this.get<GetUserProfileSummaryResponse>(
       `${this.ROUTE_V1}/${request.uid}/profile/summary`
     );
-  }
+  };
 
-  public getMyProfileSummary() {
+  public getMyProfileSummary = () => {
     return this.get<GetMyProfileSummaryResponse>(`${this.ROUTE_V1}/me/profile/summary`);
-  }
+  };
 
-  public getMyAvatarBodies() {
+  public getMyAvatarBodies = () => {
     return this.get<AvatarBody[]>(`${this.ROUTE_V1}/me/profile/avatar/bodies`);
-  }
+  };
 
-  public getMyAvatarFaces() {
+  public getMyAvatarFaces = () => {
     return this.get<AvatarFace[]>(`${this.ROUTE_V1}/me/profile/avatar/faces`);
-  }
+  };
 
-  public updateMyWallet(request: UpdateMyWalletRequest) {
+  public updateMyWallet = (request: UpdateMyWalletRequest) => {
     return this.put<void>(`${this.ROUTE_V1}/me/profile/wallet`, request);
-  }
+  };
 
-  public updateMyBio(request: UpdateMyBioRequest) {
+  public updateMyBio = (request: UpdateMyBioRequest) => {
     return this.put<void>(`${this.ROUTE_V1}/me/profile/bio`, request);
-  }
+  };
 
-  public updateMyAvatarFace(request: UpdateMyAvatarFaceRequest) {
+  public updateMyAvatarFace = (request: UpdateMyAvatarFaceRequest) => {
     return this.put<void>(`${this.ROUTE_V1}/me/profile/avatar/face`, request);
-  }
+  };
 
-  public updateMyAvatarBody(request: UpdateMyAvatarBodyRequest) {
+  public updateMyAvatarBody = (request: UpdateMyAvatarBodyRequest) => {
     return this.put<void>(`${this.ROUTE_V1}/me/profile/avatar/body`, request);
-  }
+  };
 }
 
 const instance = new UsersService();
