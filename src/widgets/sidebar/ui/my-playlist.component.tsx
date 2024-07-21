8a397f95af4304c2bb9372e30bd3db956575bc48
Will Kim <will@chequer.io>
2024-07-21 19:54:03 +0900

    feat(playlist): interactable 대응

--- src/widgets/sidebar/ui/my-playlist.component.tsx
+++ src/widgets/sidebar/ui/my-playlist.component.tsx
@@ -21,7 +21,7 @@ const MyPlaylist = () => {
   const containerCommonProps: DrawerProps = {
     title: t.playlist.title.my_playlist,
     isOpen: playlistDrawer.open,
-    close: () => setPlaylistDrawer({ open: false }),
+    close: playlistDrawer.interactable ? () => setPlaylistDrawer({ open: false }) : undefined,
     style: { zIndex: playlistDrawer.zIndex },
   };
 
@@ -55,21 +55,22 @@ const MyPlaylist = () => {
     }
   }, [playlistDrawer.open]);
 
-  // TODO: interactable 대응
   if (playlistDrawer.selectedPlaylist) {
     return (
       <Drawer
         {...containerCommonProps}
         HeaderExtra={
-          <TextButton onClick={unselectPlaylist} Icon={<PFArrowLeft width={24} height={24} />} />
+          playlistDrawer.interactable ? (
+            <TextButton onClick={unselectPlaylist} Icon={<PFArrowLeft width={24} height={24} />} />
+          ) : null
         }
       >
-        <div className='flexRow justify-between items-center mt-10 mb-6'>
-          <div className='flexRow gap-3'>
+        {playlistDrawer.interactable && (
+          <div className='flexRow justify-between items-center mb-6'>
+            {/* TODO: 여기 버튼으로 열리는 music search는 재생목록 선택 없이 즉시 추가되어야 함 */}
             <AddMusicsToPlaylistButton />
           </div>
-          <TextButton onClick={() => setEditMode(true)}>{t.common.btn.settings}</TextButton>
-        </div>
+        )}
 
         <PlaylistListItem
           key={playlistDrawer.selectedPlaylist.id}
@@ -79,15 +80,18 @@ const MyPlaylist = () => {
           })}
         />
 
-        <MusicsInPlaylist playlist={playlistDrawer.selectedPlaylist} />
+        <div className='space-y-3 [&>:first-child]:pt-3'>
+          <MusicsInPlaylist playlist={playlistDrawer.selectedPlaylist} />
+        </div>
       </Drawer>
     );
   }
 
   if (editMode) {
+    // NOTE: playlistDrawer.interactable일 때 editMode가 될 수 없다 가정
     return (
       <Drawer {...containerCommonProps}>
-        <div className='flexRow justify-between items-center mt-10 mb-6'>
+        <div className='flexRow justify-between items-center mb-6'>
           <RemovePlaylistButton targetIds={removeTargets} onSuccess={() => setRemoveTargets([])} />
           <TextButton className='text-red-300' onClick={() => setEditMode(false)}>
             {t.common.btn.complete}
@@ -101,15 +105,17 @@ const MyPlaylist = () => {
 
   return (
     <Drawer {...containerCommonProps}>
-      <div className='flexRow justify-between items-center mt-10 mb-6'>
-        <div className='flexRow gap-3'>
-          <AddMusicsToPlaylistButton />
-          <AddPlaylistButton />
+      {playlistDrawer.interactable && (
+        <div className='flexRow justify-between items-center mb-6'>
+          <div className='flexRow gap-3'>
+            <AddMusicsToPlaylistButton />
+            <AddPlaylistButton />
+          </div>
+          <TextButton onClick={() => setEditMode(true)}>{t.common.btn.settings}</TextButton>
         </div>
-        <TextButton onClick={() => setEditMode(true)}>{t.common.btn.settings}</TextButton>
-      </div>
+      )}
 
-      <Playlists onClickItem={selectPlaylist} />
+      <Playlists onClickItem={playlistDrawer.interactable ? selectPlaylist : undefined} />
     </Drawer>
   );
 };
