30fef0e7c76820e1a474f59af9fdf3b04c5e19d4
Will Kim <will@chequer.io>
2024-08-03 21:47:43 +0900

    refactor(entities/ui-state): initialValues를 zustand api 활용하도록 변경 (#149)

--- src/entities/ui-state/model/ui-state.model.tsx
+++ src/entities/ui-state/model/ui-state.model.tsx
@@ -1,7 +1,5 @@
-import { ReadonlyDeep } from 'type-fest';
 import { Playlist } from '@/shared/api/http/types/playlists';
 import type { Next } from '@/shared/lib/functions/update';
-import theme from '@/shared/ui/foundation/theme';
 
 type PlaylistDrawerState = {
   open: boolean;
@@ -14,12 +12,3 @@ export type Model = {
   playlistDrawer: PlaylistDrawerState;
   setPlaylistDrawer: (v: Next<PlaylistDrawerState>) => void;
 };
-
-export const initialValues: ReadonlyDeep<Pick<Model, 'playlistDrawer'>> = Object.freeze({
-  playlistDrawer: Object.freeze({
-    open: false,
-    interactable: true,
-    zIndex: theme.zIndex.drawer,
-    selectedPlaylist: undefined,
-  }),
-});
