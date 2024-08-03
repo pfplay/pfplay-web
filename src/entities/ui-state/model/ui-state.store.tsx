184ee1ce77a5c05aaff1d2b830793b6900446caa
Will Kim <will@chequer.io>
2024-08-03 19:06:56 +0900

    refactor(entities/ui-state): initialValues를 zustand api 활용하도록 변경

--- src/entities/ui-state/model/ui-state.store.tsx
+++ src/entities/ui-state/model/ui-state.store.tsx
@@ -4,15 +4,19 @@ import { create } from 'zustand';
 import { warnLog } from '@/shared/lib/functions/log/logger';
 import withDebugger from '@/shared/lib/functions/log/with-debugger';
 import { update } from '@/shared/lib/functions/update';
+import theme from '@/shared/ui/foundation/theme';
 import * as UIState from './ui-state.model';
 
 const logger = withDebugger(0);
 const log = logger(warnLog);
 
 export const createUIStateStore = () => {
-  return create<UIState.Model>((set) => ({
+  return create<UIState.Model>((set, _, api) => ({
     playlistDrawer: {
-      ...UIState.initialValues.playlistDrawer,
+      open: false,
+      interactable: true,
+      zIndex: theme.zIndex.drawer,
+      selectedPlaylist: undefined,
     },
     setPlaylistDrawer: (v) => {
       return set((state) => {
@@ -22,10 +26,12 @@ export const createUIStateStore = () => {
           resetValues('interactable', 'zIndex', 'selectedPlaylist');
 
           function resetValues<K extends keyof UIState.Model['playlistDrawer']>(...keys: K[]) {
+            const initialValues = api.getInitialState().playlistDrawer;
+
             keys.forEach((key) => {
-              if (updated[key] !== UIState.initialValues.playlistDrawer[key]) {
+              if (updated[key] !== initialValues[key]) {
                 log(`플레이리스트가 닫혀 ${key}가 초기 값으로 변경됩니다.`);
-                updated[key] = UIState.initialValues.playlistDrawer[key];
+                updated[key] = initialValues[key];
               }
             });
           }
