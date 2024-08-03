223adc34bfb0e4eae8c51181f7a847f9324537ca
Will Kim <will@chequer.io>
2024-08-03 22:36:52 +0900

    feat(features/playlist/list): 언어가 변경될 때마다 해당 언어에 맞춰 Grab Playlist의 이름 덮어씌우기 (#147)
    feat(features/playlist/list): 언어가 변경될 때마다 해당 언어에 맞춰 Grab Playlist의 이름을 덮어씌우기

--- src/features/playlist/list/index.ts
+++ src/features/playlist/list/index.ts
@@ -1,4 +1,4 @@
 export { default as EditablePlaylists } from './ui/editable-list.component';
 export { default as Playlists } from './ui/list.component';
 export { default as PlaylistListItem } from './ui/list-item.component';
-export { useFetchPlaylists } from './api/use-fetch-playlists.query';
+export { default as useFetchPlaylists } from './api/use-fetch-playlists.query';
