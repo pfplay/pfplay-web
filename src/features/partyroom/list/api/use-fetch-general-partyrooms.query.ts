a3d8c8d13d4847f898a1e15488c3b63d5ea217cd
Will Kim <will@chequer.io>
2024-08-04 19:11:37 +0900

    chore: 파티룸 목록 더 자주 refetch 하도록 설정

--- src/features/partyroom/list/api/use-fetch-general-partyrooms.query.ts
+++ src/features/partyroom/list/api/use-fetch-general-partyrooms.query.ts
@@ -5,14 +5,15 @@ import PartyroomsService from '@/shared/api/http/services/partyrooms';
 import { StageType } from '@/shared/api/http/types/@enums';
 import { APIError } from '@/shared/api/http/types/@shared';
 import { PartyroomSummary } from '@/shared/api/http/types/partyrooms';
-import { FIVE_MINUTES, ONE_SECOND } from '@/shared/config/time';
+import { FIVE_MINUTES } from '@/shared/config/time';
 
 export const useFetchGeneralPartyrooms = () => {
   return useQuery<PartyroomSummary[], AxiosError<APIError>, PartyroomSummary[]>({
     queryKey: [QueryKeys.PartyroomList],
     queryFn: PartyroomsService.getList,
     select: (data) => data.filter((partyroom) => partyroom.stageType === StageType.GENERAL),
-    staleTime: 10 * ONE_SECOND,
+    staleTime: 0,
     gcTime: FIVE_MINUTES,
+    refetchOnWindowFocus: true,
   });
 };
--- src/features/partyroom/list/api/use-fetch-main-partyroom.query.ts
+++ src/features/partyroom/list/api/use-fetch-main-partyroom.query.ts
@@ -5,7 +5,7 @@ import PartyroomsService from '@/shared/api/http/services/partyrooms';
 import { StageType } from '@/shared/api/http/types/@enums';
 import { APIError } from '@/shared/api/http/types/@shared';
 import { PartyroomSummary } from '@/shared/api/http/types/partyrooms';
-import { FIVE_MINUTES, ONE_SECOND } from '@/shared/config/time';
+import { FIVE_MINUTES } from '@/shared/config/time';
 
 export const useSuspenseFetchMainPartyroom = () => {
   return useSuspenseQuery<PartyroomSummary[], AxiosError<APIError>, PartyroomSummary>({
@@ -15,7 +15,8 @@ export const useSuspenseFetchMainPartyroom = () => {
       // NOTE: 메인 파티룸은 항상 존재한다고 가정
       return data.find((partyroom) => partyroom.stageType === StageType.MAIN) as PartyroomSummary;
     },
-    staleTime: 10 * ONE_SECOND,
+    staleTime: 0,
     gcTime: FIVE_MINUTES,
+    refetchOnWindowFocus: true,
   });
 };
