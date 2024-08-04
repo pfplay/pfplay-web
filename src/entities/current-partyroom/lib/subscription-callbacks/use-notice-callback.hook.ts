ff4ee15742aa4480d50a507148f816e2b25899dd
Will Kim <will@chequer.io>
2024-08-04 14:38:10 +0900

    feat: NoticeEvent 콜백 구현

--- src/entities/current-partyroom/lib/subscription-callbacks/use-notice-callback.hook.ts
+++ src/entities/current-partyroom/lib/subscription-callbacks/use-notice-callback.hook.ts
@@ -1,8 +1,15 @@
 import { useCallback } from 'react';
 import { NoticeEvent } from '@/shared/api/websocket/types/partyroom';
+import { useStores } from '@/shared/lib/store/stores.context';
 
 export default function useNoticeCallback() {
-  return useCallback((event: NoticeEvent) => {
-    console.log('NoticeEvent:', event);
-  }, []);
+  const { useCurrentPartyroom } = useStores();
+  const updateNotice = useCurrentPartyroom((state) => state.updateNotice);
+
+  return useCallback(
+    (event: NoticeEvent) => {
+      updateNotice(event.content);
+    },
+    [updateNotice]
+  );
 }
