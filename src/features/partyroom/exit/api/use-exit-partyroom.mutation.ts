786ddf932368a8a1aa26f96caf9a77393c82b270
Will Kim <will@chequer.io>
2024-08-27 01:58:56 +0900

    refactor(featues/partyroom/exit): 전반적으로 개선
    - api 세그먼트와 lib 세그먼트 구분 명확히 함
    - success 시 Participants도 removeQueries하도록 추가
    - onBeforePartyroomPageUnload > onBeforeUnload 이름 변경하고 설명 추가, 직접 슬라이스 밖으로 내보내지 않고 useExitPartyroom return문에 포함시키기

--- src/features/partyroom/exit/api/use-exit-partyroom.mutation.ts
+++ src/features/partyroom/exit/api/use-exit-partyroom.mutation.ts
@@ -1,37 +1,25 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { usePartyroomClient } from '@/entities/partyroom-client';
 import { QueryKeys } from '@/shared/api/http/query-keys';
 import PartyroomsService from '@/shared/api/http/services/partyrooms';
 import { APIError } from '@/shared/api/http/types/@shared';
 import { ExitPayload } from '@/shared/api/http/types/partyrooms';
-import { useStores } from '@/shared/lib/store/stores.context';
 
 export function useExitPartyroom() {
   const queryClient = useQueryClient();
-  const client = usePartyroomClient();
-  const { useCurrentPartyroom } = useStores();
-  const resetPartyroom = useCurrentPartyroom((state) => state.reset);
 
   return useMutation<void, AxiosError<APIError>, ExitPayload>({
     mutationFn: PartyroomsService.exit,
     onSuccess: () => {
-      client.unsubscribeAll();
-      resetPartyroom();
-
       /**
        * TODO: 파티 룸 관련 캐시 모두 제거 (members, djingQueue, etc...)
        */
       queryClient.removeQueries({
         queryKey: [QueryKeys.DjingQueue],
       });
+      queryClient.removeQueries({
+        queryKey: [QueryKeys.Participants],
+      });
     },
   });
-}
-
-/**
- * 파티룸 내에서 beforeunload 이벤트가 발생하여, onSuccess 내 동작 없이 오직 exit 요청만 보낼 때 사용합니다.
- */
-export function onBeforePartyroomPageUnload(partyroomId: number) {
-  return PartyroomsService.exit({ partyroomId });
 }
