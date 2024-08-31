c2f4779be5f0e2e7d8fb11e4c8d3f77a98da8a4d
Jongok Yoon <yoonjongok1209@gmail.com>
2024-08-31 23:26:32 +0200

    feat: skip current dj track 실패 시 에러 로깅

--- src/widgets/partyroom-djing-dialog/api/use-skip-playback.mutation.ts
+++ src/widgets/partyroom-djing-dialog/api/use-skip-playback.mutation.ts
@@ -4,6 +4,11 @@ import { QueryKeys } from '@/shared/api/http/query-keys';
 import DjsService from '@/shared/api/http/services/djs';
 import { APIError } from '@/shared/api/http/types/@shared';
 import { SkipPlaybackPayload } from '@/shared/api/http/types/djs';
+import { errorLog } from '@/shared/lib/functions/log/logger';
+import withDebugger from '@/shared/lib/functions/log/with-debugger';
+
+const logger = withDebugger(0);
+const errorLogger = logger(errorLog);
 
 export const useSkipPlayback = () => {
   const queryClient = useQueryClient();
@@ -14,6 +19,9 @@ export const useSkipPlayback = () => {
       queryClient.invalidateQueries({
         queryKey: [QueryKeys.DjingQueue, variables.partyroomId],
       });
+    },
+    onError: (error) => {
+      errorLogger('Skip current dj track failed: ', error);
     },
   });
 };
