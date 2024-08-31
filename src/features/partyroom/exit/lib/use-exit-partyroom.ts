f007192f949fc3b1d6927ab0074c3c66f43509cc
Will Kim <will@chequer.io>
2024-08-31 20:35:15 +0900

    fix(features/partyroom/exit): 파티룸 페이지에서 뒤로가기 누를 시 구독 해제 안되는 문제
    - mutation을 들고 있는 컴포넌트가 언마운트 되면 onSuccess가 실행되지 않음

--- src/features/partyroom/exit/lib/use-exit-partyroom.ts
+++ src/features/partyroom/exit/lib/use-exit-partyroom.ts
@@ -1,5 +1,4 @@
 import { usePartyroomClient } from '@/entities/partyroom-client';
-import PartyroomsService from '@/shared/api/http/services/partyrooms';
 import { useStores } from '@/shared/lib/store/stores.context';
 import { useExitPartyroom as useExitPartyroomMutation } from '../api/use-exit-partyroom.mutation';
 
@@ -9,28 +8,12 @@ export function useExitPartyroom(partyroomId: number) {
   const resetPartyroomStore = useCurrentPartyroom((state) => state.reset);
   const { mutate: exit } = useExitPartyroomMutation();
 
-  return {
-    exit: () => {
-      exit(
-        { partyroomId },
-        {
-          onSuccess: () => {
-            client.unsubscribeCurrentRoom();
-            resetPartyroomStore();
-          },
-        }
-      );
-    },
-    /**
-     * 페이지 이탈 시에 사용
-     * (페이지 이탈 시엔 쿼리 캐시 제거, 소켓 구독 해제 등의 후처리 동작이 필요 없음)
-     */
-    onBeforeUnload: () => {
-      try {
-        return PartyroomsService.exit({ partyroomId });
-      } catch {
-        /* Do nothing */
-      }
-    },
+  return () => {
+    exit({ partyroomId });
+
+    // NOTE: mutation을 들고 있는 컴포넌트가 언마운트 되면 onSuccess가 실행되지 않으므로,
+    // 이하 로직을 exit의 onSuccess에 넣으면 안됩니다
+    client.unsubscribeCurrentRoom();
+    resetPartyroomStore();
   };
 }
