a757c98c692e59c2033d7cf093c359b61856ff98
Will Kim <will@chequer.io>
2024-09-01 21:41:26 +0900

    feat: 좋아요, 싫어요, 그랩 현황에 대한 로컬 state 추가

--- src/widgets/partyroom-display-board/ui/parts/action-buttons.component.tsx
+++ src/widgets/partyroom-display-board/ui/parts/action-buttons.component.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useRef, useState } from 'react';
 import { useEvaluateCurrentPlayback } from '@/features/partyroom/evaluate-current-playback';
 import { useGrabCurrentPlayback } from '@/features/partyroom/grab-current-playback';
 import { ReactionType } from '@/shared/api/http/types/@enums';
 import { PartyroomReaction } from '@/shared/api/http/types/partyrooms';
+import { Next, update } from '@/shared/lib/functions/update';
 import { useStores } from '@/shared/lib/store/stores.context';
 import { PFPlaylistAdd, PFThumbDownAlt, PFThumbUpAlt } from '@/shared/ui/icons';
 import ActionButton from './action-button.component';
@@ -9,47 +11,98 @@ import ActionButton from './action-button.component';
 export default function ActionButtons() {
   const { mutate: grabCurrentPlayback } = useGrabCurrentPlayback();
   const { mutate: evaluateCurrentPlayback } = useEvaluateCurrentPlayback();
-  const { useCurrentPartyroom } = useStores();
-  const reaction = useCurrentPartyroom((state) => state.reaction) ?? emptyReaction;
+  const [reaction, playbackActivated] = useStores().useCurrentPartyroom((state) => [
+    state.reaction,
+    state.playbackActivated,
+  ]);
+  const [activeState, updateActiveState] = useActiveState(reaction?.history);
 
   return (
     <>
       <ActionButton
         icon={<PFThumbUpAlt width={18} height={18} />}
-        text={reaction.aggregation.likeCount}
-        active={reaction.history.like}
+        text={reaction?.aggregation.likeCount ?? 0}
+        disabled={!playbackActivated}
+        active={activeState.like}
         activeColor='red'
-        onClick={() => evaluateCurrentPlayback(ReactionType.LIKE)}
+        onClick={() => {
+          if (!playbackActivated || activeState.like) return;
+          evaluateCurrentPlayback(ReactionType.LIKE, {
+            onSuccess: () => {
+              updateActiveState({
+                like: true,
+                dislike: false,
+              });
+            },
+          });
+        }}
       />
       <ActionButton
         icon={<PFPlaylistAdd width={18} height={18} />}
-        text={reaction.aggregation.grabCount}
-        disabled={reaction.history.grab} // NOTE: grab은 끌 수 없음
-        active={reaction.history.grab}
+        text={reaction?.aggregation.grabCount ?? 0}
+        disabled={!playbackActivated || !!reaction?.history.grab} // NOTE: grab은 끌 수 없음
+        active={activeState.grab}
         activeColor='green'
-        onClick={grabCurrentPlayback}
+        onClick={() => {
+          if (!playbackActivated || activeState.grab) return;
+          grabCurrentPlayback(undefined, {
+            onSuccess: () => {
+              updateActiveState({
+                like: true,
+                grab: true,
+                dislike: false,
+              });
+            },
+          });
+        }}
       />
       <ActionButton
         icon={<PFThumbDownAlt width={18} height={18} />}
-        text={reaction.aggregation.dislikeCount}
-        active={reaction.history.dislike}
+        text={reaction?.aggregation.dislikeCount ?? 0}
+        disabled={!playbackActivated}
+        active={activeState.dislike}
         activeColor='white'
-        onClick={() => evaluateCurrentPlayback(ReactionType.DISLIKE)}
+        onClick={() => {
+          if (!playbackActivated || activeState.dislike) return;
+          evaluateCurrentPlayback(ReactionType.DISLIKE, {
+            onSuccess: () => {
+              updateActiveState({
+                like: false,
+                dislike: true,
+              });
+            },
+          });
+        }}
       />
     </>
   );
 }
 
-const emptyReaction: PartyroomReaction = {
-  history: {
-    like: false,
-    dislike: false,
-    grab: false,
-  },
-  aggregation: {
-    likeCount: 0,
-    dislikeCount: 0,
-    grabCount: 0,
-  },
-  motion: [],
-};
+/**
+ * history는 setup에서 받아온 뒤 따로 업데이트 이벤트 등이 오지 않기에, local state로 관리해야합니다.
+ */
+function useActiveState(history?: PartyroomReaction['history']) {
+  const initialized = useRef(!!history);
+  const [activeState, setActiveState] = useState(
+    history ?? {
+      like: false,
+      dislike: false,
+      grab: false,
+    }
+  );
+
+  const updateActiveState = (next: Next<PartyroomReaction['history']>) => {
+    setActiveState((prev) => {
+      return update(prev, next);
+    });
+  };
+
+  useEffect(() => {
+    if (!initialized.current && history) {
+      setActiveState(history);
+      initialized.current = true;
+    }
+  }, [history]);
+
+  return [activeState, updateActiveState] as const;
+}
