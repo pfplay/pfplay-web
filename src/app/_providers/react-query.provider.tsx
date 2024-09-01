e0bef4226613f31c211afaf048631075bb43ec3b
Will Kim <will@chequer.io>
2024-09-01 16:27:53 +0900

    chore(app): mutation error도 글로벌에서 처리 (#169)

--- src/app/_providers/react-query.provider.tsx
+++ src/app/_providers/react-query.provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ReactNode } from 'react';
-import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { getErrorMessage } from '@/shared/api/get-error-message';
@@ -44,6 +44,9 @@ function makeQueryClient() {
       },
     },
     queryCache: new QueryCache({
+      onError: handleBubbledError,
+    }),
+    mutationCache: new MutationCache({
       onError: handleBubbledError,
     }),
   });
