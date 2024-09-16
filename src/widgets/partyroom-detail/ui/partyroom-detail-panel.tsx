684f16dc83eef3b1a030b2c78a083eaa4b323259
Jongok Yoon <yoonjongok1209@gmail.com>
2024-09-16 14:58:58 +0200

    feat: share partyroom dialog 완성

--- src/widgets/partyroom-detail/ui/partyroom-detail-panel.tsx
+++ src/widgets/partyroom-detail/ui/partyroom-detail-panel.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
 import { useParams } from 'next/navigation';
 
+import { useState } from 'react';
 import { useFetchPartyroomSummary } from '@/features/partyroom/get-summary';
 import { useFetchDjingQueue } from '@/features/partyroom/list-djing-queue';
 import { StageType } from '@/shared/api/http/types/@enums';
 import { useI18n } from '@/shared/lib/localization/i18n.context';
 import { Button } from '@/shared/ui/components/button';
+import { useDialog } from '@/shared/ui/components/dialog';
 import { DjListItem } from '@/shared/ui/components/dj-list-item';
 import { Typography } from '@/shared/ui/components/typography';
 import { PFChevronRight, PFClose, PFLink, PFSettings } from '@/shared/ui/icons';
@@ -14,11 +17,61 @@ interface Props {
 }
 export default function PartyroomDetailPanel({ onClose }: Props) {
   const t = useI18n();
+  const { openDialog } = useDialog();
   const params = useParams<{ id: string }>();
   const { data: djingQueue } = useFetchDjingQueue({ partyroomId: Number(params.id) }, true);
   const [currentDj] = djingQueue?.djs.slice().sort((a, b) => a.orderNumber - b.orderNumber) || [];
 
   const { data: partyroom } = useFetchPartyroomSummary(Number(params.id));
+
+  const handleShareBtnClick = () => {
+    return openDialog(() => ({
+      title: t.common.btn.share,
+      titleAlign: 'center',
+      showCloseIcon: true,
+      classNames: {
+        container: 'w-[420px] py-8 px-10 bg-gray-800',
+      },
+      Body: () => {
+        const [isCopied, setIsCopied] = useState(false);
+
+        const sharedUrl = `https://pfplay.io/shared/${params.id}`;
+
+        const handleCopyLink = () => {
+          navigator.clipboard.writeText(sharedUrl);
+          setIsCopied(true);
+        };
+
+        return (
+          <>
+            <div className='w-[340px] flexCol gap-3'>
+              <div className='w-full h-12 flexRowCenter gap-2 bg-gray-700 rounded cursor-pointer'>
+                <Image src={'/images/ETC/twitter.png'} alt='twitter' width={24} height={24} />
+                {t.party.btn.share_twitter}
+              </div>
+              <div className='flex justify-between items-center gap-3'>
+                <div className='w-[245px] h-12 py-3 pl-3 pr-6 flexRowCenter gap-2 bg-gray-700 rounded'>
+                  <Typography type='body3' overflow='ellipsis'>
+                    {sharedUrl}
+                  </Typography>
+                </div>
+                <Button
+                  size='lg'
+                  variant='outline'
+                  color='secondary'
+                  className='w-[83px]'
+                  onClick={handleCopyLink}
+                >
+                  {/* 추후 i18n (en) Copy/Copied!로 변경 되면 btn text overflow 되는 ui defect 사라짐 */}
+                  {isCopied ? t.party.para.copy_completed : t.party.btn.copy_link}
+                </Button>
+              </div>
+            </div>
+          </>
+        );
+      },
+    }));
+  };
 
   return (
     <div className='absolute top-3 right-[108%] w-[280px] flexCol gap-6 bg-black py-7 px-5'>
@@ -61,7 +114,14 @@ export default function PartyroomDetailPanel({ onClose }: Props) {
         <PFChevronRight width={24} height={24} />
       </div>
       <div className='flexRowCenter gap-3'>
-        <Button size='sm' variant='outline' color='secondary' Icon={<PFLink />} className='flex-1'>
+        <Button
+          onClick={handleShareBtnClick}
+          size='sm'
+          variant='outline'
+          color='secondary'
+          Icon={<PFLink />}
+          className='flex-1'
+        >
           {t.common.btn.share}
         </Button>
         <Button
