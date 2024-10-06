345a8e493f0ffd92a598df28c222926e14a92637
Will Kim <will@chequer.io>
2024-10-06 18:09:50 +0900

    feat(entities/partyroom-client): CrewProfileEvent 콜백 작성

--- src/entities/partyroom-client/lib/subscription-callbacks/use-crew-profile-callback.hook.ts
+++ src/entities/partyroom-client/lib/subscription-callbacks/use-crew-profile-callback.hook.ts
@@ -1,7 +1,22 @@
 import { CrewProfileEvent } from '@/shared/api/websocket/types/partyroom';
+import { omit } from '@/shared/lib/functions/omit';
+import { useStores } from '@/shared/lib/store/stores.context';
 
 export default function useCrewProfileCallback() {
-  return (_event: CrewProfileEvent) => {
-    // TODO: implementation
+  const updateCrews = useStores().useCurrentPartyroom((state) => state.updateCrews);
+
+  return (event: CrewProfileEvent) => {
+    updateCrews((prev) => {
+      return prev.map((crew) => {
+        if (crew.crewId !== event.crewId) {
+          return crew;
+        }
+
+        return {
+          ...crew,
+          ...omit(event, 'eventType'),
+        };
+      });
+    });
   };
 }
