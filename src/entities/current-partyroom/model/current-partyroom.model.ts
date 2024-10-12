9430547355c00bbf37664cfdd322507098ff5bd0
Will Kim <will@chequer.io>
2024-10-12 15:55:10 +0900

    chore: 기존 파티룸 스토어 내 필드들에 주석 설명 추가

--- src/entities/current-partyroom/model/current-partyroom.model.ts
+++ src/entities/current-partyroom/model/current-partyroom.model.ts
@@ -19,28 +19,51 @@ export type PenaltyNotification = {
 export type Model = {
   id?: number;
 
+  /**
+   * 내 파티룸 정보
+   */
   me?: MyPartyroomInfo;
   updateMe: (next: Next<MyPartyroomInfo | undefined>) => void;
 
+  /**
+   * 플레이백 활성화 여부
+   */
   playbackActivated: boolean;
   updatePlaybackActivated: (next: boolean) => void;
 
+  /**
+   * 플레이백 정보
+   */
   playback?: PartyroomPlayback;
   updatePlayback: (next: Next<PartyroomPlayback | undefined>) => void;
 
+  /**
+   * 전광판 리액션 정보 + 모션 정보
+   */
   reaction?: PartyroomReaction;
   updateReaction: (next: Next<PartyroomReaction | undefined>) => void;
 
+  /**
+   * 현재 파티룸 내 크루 목록
+   */
   crews: Crew.Model[];
   updateCrews: (next: Next<Crew.Model[]>) => void;
 
+  /**
+   * 현재 DJ
+   */
   currentDj?: Pick<Crew.Model, 'crewId'>;
   updateCurrentDj: (next: Pick<Crew.Model, 'crewId'> | undefined) => void;
 
+  /**
+   * 전광판 공지사항
+   */
   notice: string;
   updateNotice: (next: string) => void;
 
   /**
+   * 채팅 메세지 관리 모듈
+   *
    * NOTE: chat은 zustand와는 별개의 구독 매커니즘을 가지고 있는 모듈입니다.
    * chat message list의 리액트 내 구독을 위해선 이 슬라이스에서 제공하는 useCurrentPartyroomChat hook을 사용하세요
    */
@@ -63,11 +86,18 @@ export type Model = {
   penaltyNotification?: PenaltyNotification;
   setPenaltyNotification: (notification: PenaltyNotification | undefined) => void;
 
+  /**
+   * 파티룸 입장 시 초기화하는 함수
+   */
   init: (
     next: Pick<
       Model,
       'id' | 'me' | 'playbackActivated' | 'playback' | 'reaction' | 'crews' | 'currentDj' | 'notice'
     >
   ) => void;
+
+  /**
+   * 파티룸 퇴장 시 리셋하는 함수
+   */
   reset: () => void;
 };
