15ef4d4857b5b37eee9d8e55a0d5bea4de4239b8
Will Kim <will@chequer.io>
2024-10-13 18:21:21 +0900

    feat: 다이얼로그에서 wallet provider 사용할 수 있도록 적용 범위 확장

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,13 +1,12 @@
 import { PropsWithChildren } from 'react';
 import { Header } from '@/widgets/layouts';
-import { WalletProvider } from '../_providers/wallet.provider';
 
 const SettingsLayout = ({ children }: PropsWithChildren) => {
   return (
-    <WalletProvider>
+    <>
       <Header />
       <main>{children}</main>
-    </WalletProvider>
+    </>
   );
 };
 
