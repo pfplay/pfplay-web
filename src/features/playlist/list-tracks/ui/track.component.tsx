afa0264d8acb3a30da9ab8e598cbe3faf3b6a333
Jongok Yoon <yoonjongok1209@gmail.com>
2025-01-18 22:31:57 +0100

    fix: 트랩 컴포넌트 마우스 선택 구역 수정

--- src/features/playlist/list-tracks/ui/track.component.tsx
+++ src/features/playlist/list-tracks/ui/track.component.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Image from 'next/image';
-import { MouseEvent } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { PlaylistTrack } from '@/shared/api/http/types/playlists';
@@ -8,7 +7,7 @@ import { cn } from '@/shared/lib/functions/cn';
 import { IconMenu } from '@/shared/ui/components/icon-menu';
 import { MenuItem } from '@/shared/ui/components/menu';
 import { Typography } from '@/shared/ui/components/typography';
-import { PFDragAndDrop, PFMoreVert, PFPlayCircleFilled } from '@/shared/ui/icons';
+import { PFDragAndDrop, PFMoreVert } from '@/shared/ui/icons';
 
 type TrackProps = {
   track: PlaylistTrack;
@@ -25,23 +24,18 @@ const Track = ({ track, menuItems }: TrackProps) => {
     transition,
   };
 
-  const handlePlayBtnClick = (e: MouseEvent<HTMLDivElement>, _id: number) => {
-    e.stopPropagation();
-    // id는 향후 비디오 재생 api 연결에 사용될 예정입니다.
-  };
-
   return (
     <div
       ref={setNodeRef}
-      className='relative grid grid-cols-[24px_1fr_32px] items-center gap-2'
+      className='relative grid grid-cols-[24px_1fr_32px] items-center gap-2 cursor-default'
       style={style}
       {...attributes}
     >
-      <div className='flexRowCenter w-6 h-6' {...listeners}>
+      <div className='flexRowCenter w-6 h-6 cursor-grab' {...listeners}>
         <PFDragAndDrop />
       </div>
 
-      <div className='relative w-full flexRow justify-start rounded gap-[12px] overflow-hidden'>
+      <div className='relative w-full flexRow justify-start rounded gap-[12px] overflow-hidden select-none pointer-events-none'>
         <div className='relative shrink-0 w-[80px] h-[44px] bg-gray-600'>
           <Image
             priority
@@ -51,18 +45,6 @@ const Track = ({ track, menuItems }: TrackProps) => {
             height={44}
             className={cn('w-full h-full object-contain select-none')}
           />
-          <div
-            className={cn([
-              'absolute inset-0 bg-transparent cursor-pointer z-50 flex justify-center items-center',
-              'opacity-from-zero',
-            ])}
-            onClick={(e) => {
-              e.stopPropagation();
-              handlePlayBtnClick(e, track.linkId);
-            }}
-          >
-            <PFPlayCircleFilled />
-          </div>
         </div>
         <div className='flex-1 min-w-0 select-none flexCol overflow-hidden'>
           <Typography type='caption1' overflow='ellipsis' className='text-gray-50'>
