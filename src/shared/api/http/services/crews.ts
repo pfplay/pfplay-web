f8986d2186649e605f3864a2d4a5937b1093aaaf
Will Kim <will@chequer.io>
2025-01-19 00:55:12 +0900

    chore: 서비스 신규 추가 - crewsService

---
+++ src/shared/api/http/services/crews.ts
@@ -0,0 +1,20 @@
+import { BlockCrewPayload, UnblockCrewPayload, CrewsClient } from '@/shared/api/http/types/crews';
+import { Singleton } from '@/shared/lib/decorators/singleton';
+import HTTPClient from '../client/client';
+
+@Singleton
+export default class CrewsService extends HTTPClient implements CrewsClient {
+  private ROUTE_V1 = 'v1/crews';
+
+  public getBlockedCrews() {
+    return this.get<void>(`${this.ROUTE_V1}/me/blocks`);
+  }
+
+  public blockCrew(payload: BlockCrewPayload) {
+    return this.post<void>(`${this.ROUTE_V1}/me/blocks`, payload);
+  }
+
+  public unblockCrew({ crewId }: UnblockCrewPayload) {
+    return this.delete<void>(`${this.ROUTE_V1}/me/blocks/${crewId}`);
+  }
+}
--- src/shared/api/http/services/index.ts
+++ src/shared/api/http/services/index.ts
@@ -1,8 +1,10 @@
+import CrewsService from './crews';
 import DjsService from './djs';
 import PartyroomsService from './partyrooms';
 import PlaylistsService from './playlists';
 import UsersService from './users';
 
+export const crewsService = new CrewsService();
 export const djsService = new DjsService();
 export const partyroomsService = new PartyroomsService();
 export const playlistsService = new PlaylistsService();
