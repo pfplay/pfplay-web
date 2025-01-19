5027faa0b1b31d4acfcad9aae4ecee8d1816bc63
Will Kim <will@chequer.io>
2025-01-19 14:23:40 +0900

    chore: 잘못된 네이밍 수정

--- src/features/partyroom/impose-penalty/index.ts
+++ src/features/partyroom/impose-penalty/index.ts
@@ -1,4 +1,4 @@
 export { default as useCanRemoveChatMessage } from './api/use-can-remove-chat-message.hook';
 export { default as useCanImposePenalty } from './api/use-can-impose-penalty.hook';
 export { default as useRemoveChatMessage } from './lib/use-remove-chat-message';
-export { default as useImposePenalty } from './lib/use-impost-penalty';
+export { default as useImposePenalty } from './lib/use-impose-penalty.hook';
