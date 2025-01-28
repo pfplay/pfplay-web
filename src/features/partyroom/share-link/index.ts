c67fb4067a55b539bfa873f6adf153b6bd09e1ae
Will Kim <will@chequer.io>
2025-01-28 22:46:19 +0900

    refactor: 파티룸 링크 공유 기능 features로 이동, 내부 로직 및 타입 개선

--- src/features/partyroom/share-link/index.ts
+++ src/features/partyroom/share-link/index.ts
@@ -1,1 +1,2 @@
 export { default as useGetPartyroomIdByDomain } from './api/use-get-partyroom-id-by-domain.mutation';
+export { default as useSharePartyroom } from './lib/use-open-share-dialog.hook';
