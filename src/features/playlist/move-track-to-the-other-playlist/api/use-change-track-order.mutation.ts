5c5108159fcb633383fdbbb10178ebdd127bd656
Will Kim <will@chequer.io>
2025-01-29 14:42:31 +0900

    chore: 플레이리스트 내 곡 순서 변경 시 리스트 refetch 안하도록 변경

--- src/features/playlist/move-track-to-the-other-playlist/api/use-change-track-order.mutation.ts
+++ src/features/playlist/move-track-to-the-other-playlist/api/use-change-track-order.mutation.ts
@@ -1,16 +1,18 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { QueryKeys } from '@/shared/api/http/query-keys';
+import { useMutation } from '@tanstack/react-query';
 import { playlistsService } from '@/shared/api/http/services';
 import { ChangeTrackOrderRequest } from '@/shared/api/http/types/playlists';
 
 export default function useChangeTrackOrder() {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: (params: ChangeTrackOrderRequest) =>
       playlistsService.changeTrackOrderInPlaylist(params),
-    onSuccess: (_, variables) => {
-      queryClient.invalidateQueries({ queryKey: [QueryKeys.PlaylistTracks, variables.playlistId] });
+    onSuccess: () => {
+      /**
+       * NOTE
+       *  순서 변경 할 때마다 invalidate하면, refetch 도중 찰나에 또 순서 변경 시
+       *  클라이언트 측에서 변경한 상태가 refetch 결과에 의해 일순간 롤백되는 듯 보이는 문제가 있으므로 invalidate 하지 않음
+       */
+      // queryClient.invalidateQueries({ queryKey: [QueryKeys.PlaylistTracks, variables.playlistId] });
     },
   });
 }
