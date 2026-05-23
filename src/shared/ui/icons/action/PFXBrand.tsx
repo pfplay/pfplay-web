a97a02a8d9272c882c00ebf2a97196e3024b473e
yeonny0723 <kkjuyeon@gmail.com>
2026-05-23 17:33:13 +0900

    fix(ui): X 아이콘 생성 소스 추가

--- src/shared/ui/icons/action/PFXBrand.tsx
+++ src/shared/ui/icons/action/PFXBrand.tsx
@@ -1,5 +1,4 @@
 import type { SVGProps } from 'react';
-
 const PFXBrand = (props: SVGProps<SVGSVGElement>) => (
   <svg
     width={24}
@@ -15,5 +14,4 @@ const PFXBrand = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
-
 export default PFXBrand;
